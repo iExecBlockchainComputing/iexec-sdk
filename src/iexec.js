4a8bc19b2ec87ed54b22dd33b6bf2c6956091ac3
victor <victor@vbonhomme.fr>
2017-08-31 13:59:10 +0200

    support old node versions with local transpile

--- src/iexec.js
+++ src/iexec.js
@@ -7,3 +7,6 @@ const packageJSON = require('../package.json');
 new commander.Command(packageJSON.name)
   .version(packageJSON.version)
   .parse(process.argv);
+
+const test = () => console.log('iExec');
+test();
