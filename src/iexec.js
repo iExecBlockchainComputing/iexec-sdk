3f96aea6d47a1e095600211786a61862efce6626
victor <victor@vbonhomme.fr>
2017-08-31 13:59:50 +0200

    remove unused test

--- src/iexec.js
+++ src/iexec.js
@@ -7,6 +7,3 @@ const packageJson = require('../package.json');
 new commander.Command(packageJson.name)
   .version(packageJson.version)
   .parse(process.argv);
-
-const test = () => console.log('iExec');
-test();
