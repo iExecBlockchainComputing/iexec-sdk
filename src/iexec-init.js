64fc57dfe39758c744ef2cc7f606e6d2c09c1570
victor <victor@vbonhomme.fr>
2017-09-11 15:44:47 +0200

    remove unneeded debug

--- src/iexec-init.js
+++ src/iexec-init.js
@@ -1,5 +1,20 @@
 const Debug = require('debug');
+const path = require('path');
+const util = require('util');
+const { exec } = require('child_process');
 
+const execAsync = util.promisify(exec);
 const debug = Debug('iexec-init');
+const rootPath = path.resolve(__dirname, '..');
+const trufflePath = path.join(rootPath, 'node_modules', 'truffle', 'build', 'cli.bundled.js');
 
-debug('iexec init !!!');
+async function truffleInit() {
+  const { stdout, stderr } = await execAsync(`${trufflePath} init`);
+  debug('stdout:', stdout);
+  debug('stderr:', stderr);
+}
+
+async function init() {
+  await truffleInit();
+}
+init();
