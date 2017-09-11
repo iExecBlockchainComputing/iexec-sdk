e3812cbe69053364432c5677f9252f499612beeb
victor <victor@vbonhomme.fr>
2017-09-11 18:10:02 +0200

    clone sample + clone oracle + copy oracleAPI

--- src/iexec-init.js
+++ src/iexec-init.js
@@ -1,20 +1,30 @@
 const Debug = require('debug');
-const path = require('path');
 const util = require('util');
 const { exec } = require('child_process');
+const cli = require('commander');
+const fs = require('fs-extra');
+const copy = require('recursive-copy');
 
 const execAsync = util.promisify(exec);
 const debug = Debug('iexec-init');
-const rootPath = path.resolve(__dirname, '..');
-const trufflePath = path.join(rootPath, 'node_modules', 'truffle', 'build', 'cli.bundled.js');
 
-async function truffleInit() {
-  const { stdout, stderr } = await execAsync(`${trufflePath} init`);
-  debug('stdout:', stdout);
-  debug('stderr:', stderr);
-}
+const IEXEC_GITHUB = 'git@github.com:iExecBlockchainComputing/';
+const SAMPLES_REPO = 'iexec-dapp-samples.git';
+const ORACLE_REPO = 'iexec-oracle';
+
+cli.parse(process.argv);
+
+debug('cli', cli.args);
 
 async function init() {
-  await truffleInit();
+  const branchName = cli.args.length ? cli.args[0] : 'hello-world';
+
+  await execAsync(`git clone --depth=1 -b ${branchName} ${IEXEC_GITHUB}${SAMPLES_REPO} .`);
+  await fs.remove('./.git');
+
+  await execAsync(`git clone --depth=1 ${IEXEC_GITHUB}${ORACLE_REPO} temp`);
+
+  await copy('./temp/contracts', './contracts');
+  await fs.remove('./temp');
 }
 init();
