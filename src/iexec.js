07b06c8485bdb6f5254f351af247ea01f294e2be
victor <victor@vbonhomme.fr>
2017-09-11 13:09:43 +0200

    Show debug example

--- src/iexec.js
+++ src/iexec.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+const Debug = require('debug');
 const commander = require('commander');
-
 const packageJSON = require('../package.json');
+
+const debug = Debug('iexec');
+
+debug('add DEBUG=* to show logs');
 
 new commander.Command(packageJSON.name)
   .version(packageJSON.version)
