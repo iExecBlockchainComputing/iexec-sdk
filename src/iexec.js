fa46db6972590ea4f11e2dd53fcfa38d2228e5e5
victor <victor@vbonhomme.fr>
2017-09-12 12:37:15 +0200

    remove alias, remove lines

--- src/iexec.js
+++ src/iexec.js
@@ -12,21 +12,12 @@ cli
   .version(packageJSON.version)
 ;
 
-cli
-  .command('init', 'init sample iexec dapp')
-;
+cli.command('init', 'init sample iexec dapp');
 
-cli
-  .command('truffle [args...]', 'execute truffle with any number of truffle arguments')
-  .alias('t')
-;
+cli.command('truffle [args...]', 'execute truffle with any number of truffle arguments');
 
-cli
-  .command('compile [args...]', 'call truffle compile')
-;
+cli.command('compile [args...]', 'call truffle compile');
 
-cli
-  .command('migrate [args...]', 'call truffle migrate')
-;
+cli.command('migrate [args...]', 'call truffle migrate');
 
 cli.parse(process.argv);
