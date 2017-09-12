c703ae7ac609eeeb90c9b199a5b6b24bcffc3f1b
victor <victor@vbonhomme.fr>
2017-09-12 12:22:41 +0200

    add debug log

--- src/truffle-cli.js
+++ src/truffle-cli.js
@@ -9,6 +9,8 @@ const rootPath = path.resolve(__dirname, '..');
 const trufflePath = path.join(rootPath, 'node_modules', 'truffle', 'build', 'cli.bundled.js');
 
 const spawnAsync = (bin, args) => new Promise((resolve, reject) => {
+  debug('spawnAsync bin', bin);
+  debug('spawnAsync args', args);
   const proc = args ? spawn(bin, args) : spawn(bin);
 
   proc.stdout.on('data', data => console.log(`${data}`));
