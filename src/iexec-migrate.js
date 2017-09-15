71b4a32f6d270f1e79ce69118c0dc0efd60232b2
victor <victor@vbonhomme.fr>
2017-09-15 12:26:03 +0200

    implement iexec migrate with support for local wallet

--- src/iexec-migrate.js
+++ src/iexec-migrate.js
@@ -8,14 +8,14 @@ const cli = require('commander');
 const path = require('path');
 const tx = require('@warren-bank/ethereumjs-tx-sign');
 const truffle = require('./truffle-cli');
+const wallet = require('./wallet');
 // eslint-disable-next-line
 const truffleConfig = require(path.join(process.cwd(), 'truffle.js'));
+// eslint-disable-next-line
+const iexecConfig = require(path.join(process.cwd(), 'iexec.js'));
 
 const debug = Debug('iexec:iexec-migrate');
 const readFileAsync = Promise.promisify(fs.readFile);
-
-const privateKey = '';
-const publicAddr = '';
 
 cli
   .option('--network [name]', 'migrate to network name', 'ropsten')
@@ -23,40 +23,54 @@ cli
   .parse(process.argv);
 
 const migrate = async () => {
-  const web3 = new Web3(new Web3.providers.HttpProvider(truffleConfig.networks[cli.network].host));
+  const network = truffleConfig.networks[cli.network];
+  const web3 = new Web3(new Web3.providers.HttpProvider(network.host));
 
-  await truffle('compile');
+  await truffle.compile();
 
-  const compiledFile = await readFileAsync('build/contracts/HelloWorld.json');
+  console.log(`Deploying ${iexecConfig.name} contract...`);
+  const compiledFile = await readFileAsync(`build/contracts/${iexecConfig.name}.json`);
   const { abi, unlinked_binary } = JSON.parse(compiledFile);
 
   const contract = new web3.eth.Contract(abi);
 
   const unsignedTx = contract.deploy({
     data: unlinked_binary,
-    arguments: ['0xC3fb2431042fBdde67B8356AbCabBc5c14660849'],
+    arguments: [iexecConfig.oracleAddress],
   }).encodeABI();
 
-  const [gasPrice, nonce, chainId] = await Promise.all([
-    web3.eth.getGasPrice(),
-    web3.eth.getTransactionCount(publicAddr),
-    web3.eth.net.getId(),
-  ]);
-  debug('gasPrice', gasPrice);
-  debug('nonce', nonce);
-  debug('chainId', chainId);
+  if (cli.wallet === 'local') {
+    const userWallet = await wallet.load();
+    const [networkGasPrice, nonce, networkChainId] = await Promise.all([
+      web3.eth.getGasPrice(),
+      web3.eth.getTransactionCount(userWallet.address),
+      web3.eth.net.getId(),
+    ]);
+    debug('networkGasPrice', networkGasPrice);
+    debug('nonce', nonce);
 
-  const { rawTx } = tx.sign({
-    nonce: web3.utils.toHex(nonce),
-    gasPrice: web3.utils.toHex(gasPrice * 3),
-    gasLimit: web3.utils.toHex(4400000),
-    data: unsignedTx,
-    chainId,
-  }, privateKey);
+    const gasPriceMultiplier = network.gasPriceMultiplier || 3;
+    const gasPrice = network.gasPrice || networkGasPrice * gasPriceMultiplier;
+    debug('gasPrice', gasPrice);
+    const gasLimit = network.gas || 4400000;
+    debug('gasLimit', gasLimit);
+    const chainId = network.chainId || networkChainId;
+    debug('chainId', chainId);
 
-  const txReceipt = await web3.eth.sendSignedTransaction('0x'.concat(rawTx))
-    .once('transactionHash', hash => debug('hash', hash))
-    .on('error', error => debug('error', error));
-  debug('txReceipt', txReceipt);
+    const { rawTx } = tx.sign({
+      nonce: web3.utils.toHex(nonce),
+      gasPrice: web3.utils.toHex(gasPrice),
+      gasLimit: web3.utils.toHex(gasLimit),
+      data: unsignedTx,
+      chainId,
+    }, userWallet.privateKey);
+
+    // const txReceipt = await web3.eth.sendSignedTransaction('0x'.concat(rawTx))
+    //   .once('transactionHash', hash => debug('hash', hash))
+    //   .on('error', error => debug('error', error));
+    // debug('txReceipt', txReceipt);
+  } else if (cli.wallet === 'remote') {
+    debug('remote');
+  }
 };
 migrate();
