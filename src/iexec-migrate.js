fe56bb6dcf1561c02172f515025fade06ff9b02b
victor <victor@vbonhomme.fr>
2017-09-18 03:54:17 +0200

    move back to stable web3 api

--- src/iexec-migrate.js
+++ src/iexec-migrate.js
@@ -6,9 +6,11 @@ const Web3 = require('web3');
 const Promise = require('bluebird');
 const cli = require('commander');
 const path = require('path');
+const ora = require('ora');
 const tx = require('@warren-bank/ethereumjs-tx-sign');
 const truffle = require('./truffle-cli');
 const wallet = require('./wallet');
+const utils = require('./utils');
 // eslint-disable-next-line
 const truffleConfig = require(path.join(process.cwd(), 'truffle.js'));
 // eslint-disable-next-line
@@ -24,31 +26,32 @@ cli
   .parse(process.argv);
 
 const migrate = async () => {
+  const spinner = ora();
   try {
     const network = truffleConfig.networks[cli.network];
     const web3 = new Web3(new Web3.providers.HttpProvider(network.host));
+    Promise.promisifyAll(web3.eth);
 
     await truffle.compile();
 
-    console.log(`Deploying ${iexecConfig.name} contract...`);
+    spinner.start(`Deploying ${iexecConfig.name} contract...`);
+
     const compiledFileJSONPath = `build/contracts/${iexecConfig.name}.json`;
     const compiledFileJSON = await readFileAsync(compiledFileJSONPath);
     const compiledFile = JSON.parse(compiledFileJSON);
     const { abi, unlinked_binary } = compiledFile;
 
-    const contract = new web3.eth.Contract(abi);
+    const Contract = web3.eth.contract(abi);
 
-    const toDeploy = { data: unlinked_binary };
-    if (iexecConfig.constructorArgs) toDeploy.arguments = iexecConfig.constructorArgs;
+    const constructorArgs = iexecConfig.constructorArgs || [];
 
-    const unsignedTx = contract.deploy(toDeploy).encodeABI();
+    const unsignedTx = Contract.new.getData(constructorArgs, { data: unlinked_binary });
 
     if (cli.wallet === 'local') {
       const userWallet = await wallet.load();
-      const [networkGasPrice, nonce, networkChainId] = await Promise.all([
-        web3.eth.getGasPrice(),
-        web3.eth.getTransactionCount(userWallet.address),
-        web3.eth.net.getId(),
+      const [networkGasPrice, nonce] = await Promise.all([
+        web3.eth.getGasPriceAsync(),
+        web3.eth.getTransactionCountAsync('0x'.concat(userWallet.address)),
       ]);
       debug('networkGasPrice', networkGasPrice);
       debug('nonce', nonce);
@@ -58,21 +61,24 @@ const migrate = async () => {
       debug('gasPrice', gasPrice);
       const gasLimit = network.gas || 4400000;
       debug('gasLimit', gasLimit);
-      const chainId = network.chainId || networkChainId;
+      const chainId = web3.version.network;
       debug('chainId', chainId);
 
       const { rawTx } = tx.sign({
-        nonce: web3.utils.toHex(nonce),
-        gasPrice: web3.utils.toHex(gasPrice),
-        gasLimit: web3.utils.toHex(gasLimit),
+        nonce: web3.toHex(nonce),
+        gasPrice: web3.toHex(gasPrice),
+        gasLimit: web3.toHex(gasLimit),
         data: unsignedTx,
         chainId,
       }, userWallet.privateKey);
 
-      const txReceipt = await web3.eth.sendSignedTransaction('0x'.concat(rawTx))
-        .once('transactionHash', hash => console.log('txHash', hash, '\n'))
-        .on('error', error => debug('error', error));
-      console.log('txReceipt', txReceipt, '\n');
+      const txHash = await web3.eth.sendRawTransactionAsync('0x'.concat(rawTx));
+      spinner.succeed(`new contract txHash: ${txHash} \n`);
+
+      spinner.start('waiting for txReceipt');
+      const txReceipt = await utils.waitFor(web3.eth.getTransactionReceiptAsync, txHash);
+      spinner.succeed('new contract txReceipt:');
+      console.log(JSON.stringify(txReceipt, null, 4));
       console.log(`View on etherscan: https://${cli.network}.etherscan.io/tx/${txReceipt.transactionHash}\n`);
 
       compiledFile.networks[chainId] = { address: txReceipt.contractAddress };
@@ -82,7 +88,7 @@ const migrate = async () => {
       debug('remote');
     }
   } catch (error) {
-    console.log(`"iexec migrate" failed with ${error}`);
+    spinner.fail(`"iexec migrate" failed with ${error}`);
   }
 };
 migrate();
