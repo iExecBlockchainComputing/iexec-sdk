d6724a6c5996686e1642db5061909db7ee774f04
francois branciard <francois.branciard@iex.ec>
2017-10-26 19:41:58 +0200

    fix :  iexec apps deploy

--- src/iexec-deploy.js
+++ src/iexec-deploy.js
@@ -9,4 +9,4 @@ cli
   .option('--chain, --network <name>', 'migrate to network name', 'ropsten')
   .parse(process.argv);
 
-migrate(cli.network).then(() => apps.send(cli.network, cli.args[0])).catch(() => {});
+migrate(cli.network).then(() => apps.deploy(cli.network, cli.args[0])).catch(() => {});
