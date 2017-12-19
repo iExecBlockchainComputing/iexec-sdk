18ffdc901fc856314744c86e4bee4c0dbdb488e3
victor <victor@vbonhomme.fr>
2017-12-19 16:40:40 +0100

    add iexec submit --dapp address

--- src/iexec-submit.js
+++ src/iexec-submit.js
@@ -5,6 +5,7 @@ const submit = require('./submit');
 
 cli
   .option('--chain, --network <name>', 'network name', 'ropsten')
+  .option('--dapp <dappAddress>', 'submit work on a specific smart contract address')
   .parse(process.argv);
 
-submit(cli.network, 'iexecSubmit', cli.args[0]).catch(() => {});
+submit(cli.network, 'iexecSubmit', cli.args[0], cli.dapp).catch(() => {});
--- src/submit.js
+++ src/submit.js
@@ -1,52 +1,64 @@
 const Debug = require('debug');
 const ora = require('ora');
-const Web3 = require('web3');
 const Promise = require('bluebird');
 const oracleJSON = require('iexec-oracle-contract/build/contracts/IexecOracle.json');
+const escrowJSON = require('iexec-oracle-contract/build/contracts/IexecOracleEscrow.json');
 const rlcJSON = require('rlc-faucet-contract/build/contracts/FaucetRLC.json');
+const createIEXECClient = require('iexec-server-js-client');
 const wallet = require('./wallet');
 const utils = require('./utils');
 const oraOptions = require('./oraOptions');
+const account = require('./account');
 
 const debug = Debug('iexec:submit');
 
-const submit = async (networkName, methodName, param) => {
+const submit = async (chainName, methodName, param, cliDappAddress) => {
   const spinner = ora(oraOptions);
   try {
-    debug('networkName', networkName);
+    debug('cliDappAddress', cliDappAddress);
+    debug('chainName', chainName);
     debug('param', param);
     const userWallet = await wallet.load();
 
     const work = param || JSON.stringify(utils.iexecConfig.work);
     const fnString = methodName.concat('(', work, ')');
-    const network = utils.truffleConfig.networks[networkName];
 
-    const web3 = new Web3(new Web3.providers.HttpProvider(network.host));
-    Promise.promisifyAll(web3.eth);
-
+    const chain = utils.getChains()[chainName];
     const { abi, networks } = await utils.loadContractDesc();
 
-    if (!(network.network_id in networks)) throw Error(`No existing deployed contract on ${networkName}`);
-    const dappAddress = networks[network.network_id].address;
-    const dappContract = web3.eth.contract(abi).at(dappAddress);
+    if (!(chain.id in networks) && !cliDappAddress) throw Error(`No existing deployed contract on ${chainName} or no contract address provided to the cli`);
+
+    // hit iexec server REST API to create a user on the fly
+    // so it can be mandated by the bridge
+    if (cliDappAddress) {
+      const { jwtoken } = await account.load();
+      const iexec = createIEXECClient({ server: chain.server });
+      await iexec.getCookieByJWT(jwtoken);
+      const version = await iexec.get('version');
+      debug('version', version);
+    }
+
+    const dappAddress = cliDappAddress || networks[chain.id].address;
+    const dappContract = chain.web3.eth.contract(abi).at(dappAddress);
     debug('dappAddress', dappAddress);
 
     const unsignedTx = dappContract[methodName].getData(work);
     debug('unsignedTx', unsignedTx);
 
-    const rlcAddress = rlcJSON.networks[network.network_id].address;
-    const rlcContract = web3.eth.contract(rlcJSON.abi).at(rlcAddress);
+    const rlcAddress = rlcJSON.networks[chain.id].address;
+    const rlcContract = chain.web3.eth.contract(rlcJSON.abi).at(rlcAddress);
     Promise.promisifyAll(rlcContract);
 
-    const oracleAddress = oracleJSON.networks[network.network_id].address;
-    const oracleContract = web3.eth.contract(oracleJSON.abi).at(oracleAddress);
+    const escrowAddress = escrowJSON.networks[chain.id].address;
+    const oracleAddress = oracleJSON.networks[chain.id].address;
+    const oracleContract = chain.web3.eth.contract(oracleJSON.abi).at(oracleAddress);
     Promise.promisifyAll(oracleContract);
 
     spinner.start(`calling ${fnString}`);
     const [callbackPrice, dappPrice, allowance] = await Promise.all([
       oracleContract.callbackPriceAsync(),
       oracleContract.getDappPriceAsync(dappAddress),
-      rlcContract.allowanceAsync('0x'.concat(userWallet.address), oracleAddress),
+      rlcContract.allowanceAsync('0x'.concat(userWallet.address), escrowAddress),
     ]);
     debug('callbackPrice', callbackPrice.toNumber());
     debug('dappPrice', dappPrice.toNumber());
@@ -55,21 +67,21 @@ const submit = async (networkName, methodName, param) => {
     if (dappPrice > allowance) throw Error(`the dapp price (${dappPrice} nRLC) is higher than your iexec credit (${allowance} nRLC).`);
 
     const txHash = await utils.signAndSendTx({
-      web3,
+      web3: chain.web3,
       userWallet,
       unsignedTx,
-      network,
+      network: chain,
       contractAddress: dappAddress,
       value: callbackPrice,
-      chainID: network.network_id,
+      chainID: chain.id,
     });
     spinner.info(`${fnString} txHash: ${txHash} \n`);
 
     spinner.start('waiting for transaction to be mined');
-    const txReceipt = await utils.waitFor(web3.eth.getTransactionReceiptAsync, txHash);
+    const txReceipt = await utils.waitFor(chain.web3.eth.getTransactionReceiptAsync, txHash);
 
     debug('txReceipt:', JSON.stringify(txReceipt, null, 4));
-    spinner.info(`View on etherscan: https://${networkName}.etherscan.io/tx/${txReceipt.transactionHash}\n`);
+    spinner.info(`View on etherscan: https://${chainName}.etherscan.io/tx/${txReceipt.transactionHash}\n`);
     spinner.succeed('calculation job submitted to iExec workers \n');
   } catch (error) {
     spinner.fail(`"iexec submit" failed with ${error}`);
