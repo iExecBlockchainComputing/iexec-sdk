5d5951423b6049757e33afe9e6b95e0dd8308aba
victor <victor@vbonhomme.fr>
2017-12-19 11:49:06 +0100

    fix iexec result txHash

--- src/result.js
+++ src/result.js
@@ -1,9 +1,11 @@
 const Debug = require('debug');
+const fs = require('fs');
+const path = require('path');
 const Promise = require('bluebird');
 const ora = require('ora');
 const Web3 = require('web3');
 const oracleJSON = require('iexec-oracle-contract/build/contracts/IexecOracle.json');
-const createXWHEPClient = require('xwhep-js-client');
+const createIEXECClient = require('iexec-server-js-client');
 const wallet = require('./wallet');
 const utils = require('./utils');
 const oraOptions = require('./oraOptions');
@@ -45,20 +47,29 @@ const fetchResults = async (txHash, chainName, save) => {
       return;
     }
 
-    const xwhep = createXWHEPClient({ hostname: 'xw.iex.ec', port: '443' });
+    const chain = utils.getChains()[chainName];
+    debug('chain.server', chain.server);
+    const iexec = createIEXECClient({ server: chain.server });
     const { jwtoken } = await account.load();
-    const cookies = await xwhep.auth(jwtoken);
-    const work = await xwhep.getWorkByExternalID(cookies, txHash);
+    await iexec.getCookieByJWT(jwtoken);
+    const work = await iexec.getWorkByExternalID(txHash);
     debug('work.xwhep.work[0]', work.xwhep.work[0]);
-    let downResult;
-    if (save) downResult = await xwhep.download(cookies, work.xwhep.work[0].resulturi[0], txHash);
-    debug('downResult', downResult);
+    let resultPath;
+    const resultURI = iexec.getFieldValue(work, 'resulturi');
+    const resultUID = iexec.uri2uid(resultURI);
+    if (save) {
+      const resultObj = await iexec.getUID(resultUID);
+      const extension = iexec.getFieldValue(resultObj, 'type').toLowerCase();
+      resultPath = path.join(process.cwd(), txHash.concat('.', extension));
+      const resultStream = fs.createWriteStream(resultPath);
+      await iexec.downloadStream(resultUID, resultStream);
+    }
 
     spinner.succeed('Result:');
     console.log('   stdout:  ', JSON.stringify(result[2]));
     console.log('   stderr:  ', JSON.stringify(result[3]));
-    console.log('   resuri:  ', JSON.stringify(work.xwhep.work[0].resulturi[0]), '\n');
-    if (save) spinner.succeed(`Saved result to file ${downResult.savePath}`);
+    console.log('   resuri:  ', JSON.stringify(resultURI), '\n');
+    if (save) spinner.succeed(`Saved result to file ${resultPath}`);
   } catch (error) {
     spinner.fail(`"iexec result" failed with ${error}`);
     throw error;
