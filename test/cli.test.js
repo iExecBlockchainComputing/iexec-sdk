c475a2e114e49132d742d777d48a0061495e54a6
victor <victor@vbonhomme.fr>
2018-01-12 16:08:53 +0100

    test wallet show

--- test/cli.test.js
+++ test/cli.test.js
@@ -6,18 +6,22 @@ const Promise = require('bluebird');
 const execAsync = Promise.promisify(exec);
 // const TX_HASH = '0x86dafcaeab83a0505baf36482cf2ded310fd6d2b46912d6ef6fadfbce7d3d30a';
 
-test('iexec init', () => expect(execAsync('iexec init.js')).resolves.not.toBe(1), 10000);
+test('iexec init', () => expect(execAsync('iexec init')).resolves.not.toBe(1), 10000);
 
 test('iexec wallet create', () => {
   process.chdir('iexec-init');
   return expect(execAsync('iexec wallet create')).resolves.not.toBe(1);
 });
 
+test('iexec wallet show', () => expect(execAsync('iexec wallet create')).resolves.not.toBe(1));
+
 test('iexec account login', () => expect(execAsync('iexec account login')).resolves.not.toBe(1));
 
 test('iexec account show', () => expect(execAsync('iexec account show')).resolves.not.toBe(1));
 
 test('iexec compile', () => expect(execAsync('iexec compile')).resolves.not.toBe(1), 20000);
+
+test('iexec truffle help', () => expect(execAsync('iexec truffle help')).resolves.not.toBe(1));
 
 // test('iexec result <txHash>', () => expect(execAsync(`iexec result ${TX_HASH}`)).resolves.not.toBe(1), 10000);
 
