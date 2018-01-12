e79e1ae8450eb99618674b091101609fcbb79701
victor <victor@vbonhomme.fr>
2018-01-12 17:51:24 +0100

    fix test

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,10 +1,8 @@
 const fs = require('fs-extra');
-const path = require('path');
 const { exec } = require('child_process');
 const Promise = require('bluebird');
 
 const execAsync = Promise.promisify(exec);
-// const TX_HASH = '0x86dafcaeab83a0505baf36482cf2ded310fd6d2b46912d6ef6fadfbce7d3d30a';
 
 test('iexec init', () => expect(execAsync('iexec init')).resolves.not.toBe(1), 10000);
 
@@ -13,7 +11,7 @@ test('iexec wallet create', () => {
   return expect(execAsync('iexec wallet create')).resolves.not.toBe(1);
 });
 
-test('iexec wallet show', () => expect(execAsync('iexec wallet create')).resolves.not.toBe(1));
+test('iexec wallet show', () => expect(execAsync('iexec wallet show')).resolves.not.toBe(1));
 
 test('iexec account login', () => expect(execAsync('iexec account login')).resolves.not.toBe(1));
 
@@ -21,8 +19,6 @@ test('iexec account show', () => expect(execAsync('iexec account show')).resolve
 
 test('iexec compile', () => expect(execAsync('iexec compile')).resolves.not.toBe(1), 20000);
 
-test('iexec truffle help', () => expect(execAsync('iexec truffle help')).resolves.not.toBe(1));
+test('iexec truffle version', () => expect(execAsync('iexec truffle version')).resolves.not.toBe(1));
 
-// test('iexec result <txHash>', () => expect(execAsync(`iexec result ${TX_HASH}`)).resolves.not.toBe(1), 10000);
-
-afterAll(() => fs.remove(path.join('.', 'iexec-init')));
+afterAll(() => fs.remove('iexec-init'));
