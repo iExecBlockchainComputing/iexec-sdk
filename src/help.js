d28ab644bd5b5583a0f7364998952a4fc5c3cd21
victor <victor@vbonhomme.fr>
2018-04-24 14:45:07 +0200

    fix args check logic

--- src/help.js
+++ src/help.js
@@ -16,13 +16,15 @@ const helpMessage = () => {
 const help = (cli, { checkNoArgs = true, checkWrongArgs = true } = {}) => {
   cli.on('--help', helpMessage);
   cli.parse(process.argv);
+  // debug('cli.args', cli.args);
+  // debug('cli._execs', cli._execs);
 
   if (checkNoArgs && cli.args.length === 0) {
     console.log('');
     console.log(colors.red('  missing argument'));
     cli.help();
   } else if (checkWrongArgs) {
-    if (typeof cli.args[0] !== 'object') {
+    if (typeof cli.args[cli.args.length - 1] !== 'object') {
       debug('not an object');
       if (!cli._execs[cli.args[0]]) {
         console.log('');
