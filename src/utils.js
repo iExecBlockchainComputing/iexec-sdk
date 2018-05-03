ed4bef33c8fd3e9469402461a4198b80c008b558
victor <victor@vbonhomme.fr>
2018-05-04 00:38:21 +0200

    clean utils

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,21 @@
 const Debug = require('debug');
 const Promise = require('bluebird');
 const fs = require('fs-extra');
-const tx = require('@warren-bank/ethereumjs-tx-sign');
 const path = require('path');
-const Web3 = require('web3');
+const EthJS = require('ethjs');
+const ethUtil = require('ethjs-util');
+const SignerProvider = require('./ethjs-iexec-signer');
+const keystore = require('./keystore');
+const createIEXECContracts = require('./iexec-contracts-js-client');
 // eslint-disable-next-line
 const truffleConfig = require(path.join(process.cwd(), 'truffle.js'));
-// eslint-disable-next-line
-const iexecConfig = require(path.join(process.cwd(), 'iexec.js'));
 
 const debug = Debug('iexec:utils');
 const readFileAsync = Promise.promisify(fs.readFile);
-const writeFileAsync = Promise.promisify(fs.writeFile);
 
 const FETCH_INTERVAL = 1000;
 const TIMEOUT = 60 * 1000;
 const sleep = ms => new Promise(res => setTimeout(res, ms));
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-const DEFAULT_GAS_PRICE_MULTIPLIER = 1;
-let DEFAULT_GAS_LIMIT_MULTIPLIER = 1;
-const BLOCK_GAS_LIMIT = 4600000;
 
 const waitFor = async (fn, hash) => {
   let counter = 0;
@@ -39,119 +35,37 @@ const waitFor = async (fn, hash) => {
   }
 };
 
-const signAndSendTx = async ({
-  chain,
-  userWallet,
-  unsignedTx,
-  contractAddress = ZERO_ADDRESS,
-  value = 0,
-  nonceOffset = 0,
-}) => {
-  try {
-    const isMigrating = contractAddress === ZERO_ADDRESS;
-    DEFAULT_GAS_LIMIT_MULTIPLIER = isMigrating ? 5 : 3;
-
-    const [networkGasPrice, nonce, estimatedGas] = await Promise.all([
-      chain.web3.eth.getGasPriceAsync(),
-      chain.web3.eth.getTransactionCountAsync('0x'.concat(userWallet.address)),
-      chain.web3.eth.estimateGasAsync({
-        value: chain.web3.toHex(value),
-        data: unsignedTx,
-        to: contractAddress,
-        from: '0x'.concat(userWallet.address),
-      }),
-    ]);
-
-    debug('contractAddress', contractAddress);
-    debug('networkGasPrice', networkGasPrice.toNumber());
-    debug('nonce', nonce);
-    debug('nonceOffset', nonceOffset);
-    debug('estimatedGas', estimatedGas);
-    debug('value', chain.web3.toHex(value));
-
-    const gasPriceMultiplier =
-      chain.gasPriceMultiplier || DEFAULT_GAS_PRICE_MULTIPLIER;
-    const gasPrice = chain.gasPrice || networkGasPrice * gasPriceMultiplier;
-    debug('gasPrice', gasPrice);
-    const gasLimitMultiplier =
-      chain.gasLimitMultiplier || DEFAULT_GAS_LIMIT_MULTIPLIER;
-    debug('network.gas', chain.gas);
-    const gasLimit = Math.min(
-      chain.gas || estimatedGas * gasLimitMultiplier,
-      BLOCK_GAS_LIMIT,
-    );
-    debug('gasLimit', gasLimit);
-    debug('chain.id', chain.id);
-
-    const txObject = {
-      nonce: chain.web3.toHex(nonce + nonceOffset),
-      gasPrice: chain.web3.toHex(gasPrice),
-      gasLimit: chain.web3.toHex(gasLimit),
-      value: chain.web3.toHex(value),
-      data: unsignedTx,
-      chainId: parseInt(chain.id, 10),
-    };
-    debug('txObject', txObject);
-    if (contractAddress !== ZERO_ADDRESS) txObject.to = contractAddress;
-    const { rawTx } = tx.sign(txObject, userWallet.privateKey);
-
-    const txHash = await chain.web3.eth.sendRawTransactionAsync('0x'.concat(rawTx));
-    return txHash;
-  } catch (error) {
-    debug('signAndSendTx()', error);
-    throw error;
-  }
-};
-
 const getChains = () => {
   try {
     const chains = {};
     const networkNames = Object.keys(truffleConfig.networks);
     networkNames.forEach((name) => {
+      const ethProvider = new SignerProvider(
+        truffleConfig.networks[name].host,
+        {
+          signTransaction: keystore.signTransaction,
+          accounts: keystore.accounts,
+        },
+      );
+
       chains[name] = Object.assign({}, truffleConfig.networks[name]);
       chains[name].name = name;
-      chains[name].web3 = new Web3(new Web3.providers.HttpProvider(truffleConfig.networks[name].host));
-      Promise.promisifyAll(chains[name].web3.eth);
+      chains[name].ethjs = new EthJS(ethProvider);
+      chains[name].EthJS = EthJS;
       chains[name].id = truffleConfig.networks[name].network_id;
       chains[chains[name].id] = chains[name];
+      chains[name].contracts = createIEXECContracts({
+        eth: chains[name].ethjs,
+        chainID: chains[name].id,
+        txOptions: {
+          from: '0x1fa8602668d4bda4f45a25c8def0a619499457db',
+          chainId: parseInt(chains[name].id, 10),
+        },
+      });
     });
     return chains;
   } catch (error) {
     debug('getChains()', error);
-    throw error;
-  }
-};
-
-const loadContractDesc = async () => {
-  try {
-    const contractDescJSONPath = path.join(
-      'build',
-      'contracts',
-      `${iexecConfig.name}.json`,
-    );
-    const contractDescJSON = await readFileAsync(contractDescJSONPath);
-    const contractDesc = JSON.parse(contractDescJSON);
-    return contractDesc;
-  } catch (error) {
-    debug('loadContractDesc()', error);
-    throw error;
-  }
-};
-
-const saveContractDesc = async (contractDesc) => {
-  try {
-    const contractDescJSONPath = path.join(
-      'build',
-      'contracts',
-      `${iexecConfig.name}.json`,
-    );
-    await writeFileAsync(
-      contractDescJSONPath,
-      JSON.stringify(contractDesc, null, 4),
-    );
-    return contractDescJSONPath;
-  } catch (error) {
-    debug('saveContractDesc()', error);
     throw error;
   }
 };
@@ -182,16 +96,97 @@ const checkTxReceipt = (txReceipt, gasLimit) => {
   }
 };
 
+const loadIEXECConf = async () => {
+  try {
+    const iexecConfPath = path.join(process.cwd(), 'iexec.json');
+    const iexecConfJSON = await readFileAsync(iexecConfPath);
+    const iexecConf = JSON.parse(iexecConfJSON);
+    debug('iexecConf', iexecConf);
+    return iexecConf;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('Aborting. You need an iexec.json config file to continue');
+    }
+    debug('loadIEXECConf() error', error);
+    throw error;
+  }
+};
+
+const getContractAddress = (desc, chainID, { strict = true } = {}) => {
+  try {
+    if (!('networks' in desc)) {
+      if (strict) {
+        throw Error('missing networks key in contract JSON description');
+      }
+      return undefined;
+    }
+    if (!(chainID in desc.networks)) {
+      if (strict) {
+        throw Error(`missing "${chainID}" key in contract JSON description`);
+      }
+      return undefined;
+    }
+    if (!('address' in desc.networks[chainID])) {
+      if (strict) {
+        throw Error(`missing address key in contract JSON description for chainID: ${chainID}`);
+      }
+      return undefined;
+    }
+    return desc.networks[chainID].address;
+  } catch (error) {
+    debug('getContractAddress()', error);
+    throw error;
+  }
+};
+
+const getRPCObjValue = (obj, { strict = true } = {}) => {
+  if (!(typeof obj === 'object')) {
+    if (strict) throw Error('RPC object is not of type object');
+    return obj;
+  }
+  if (!('0' in obj)) {
+    if (strict) throw Error('no "0" key in RPC response object');
+    return undefined;
+  }
+  return obj[0];
+};
+
+const stringifyRPCObj = (obj) => {
+  try {
+    const str = Object.keys(obj).reduce(
+      (accu, curr) =>
+        accu.concat(`\n   ${curr}: ${getRPCObjValue(obj[curr], { strict: false })}`),
+      '   ',
+    );
+    return str;
+  } catch (error) {
+    debug('stringifyRPCObj()', error);
+    throw error;
+  }
+};
+
+const isEthAddress = (address, { strict = false } = {}) => {
+  const isHexString = ethUtil.isHexString(address);
+  if (!isHexString && strict) {
+    throw Error(`address ${address} is not a valid Ethereum address`);
+  }
+  return isHexString;
+};
+
+const toUpperFirst = str => ''.concat(str[0].toUpperCase(), str.substr(1));
+
 module.exports = {
-  iexecConfig,
   truffleConfig,
   waitFor,
-  signAndSendTx,
   getChains,
-  loadContractDesc,
-  saveContractDesc,
   chainToEtherscanURL,
   getOracleWallet,
   getFaucetWallet,
   checkTxReceipt,
+  loadIEXECConf,
+  getContractAddress,
+  stringifyRPCObj,
+  isEthAddress,
+  getRPCObjValue,
+  toUpperFirst,
 };
