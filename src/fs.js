5cd14a5d6c634f4ecfe9f71bd0376dbe566c2419
victor <victor@vbonhomme.fr>
2018-05-04 22:04:23 +0200

    rename loader to js

--- src/fs.js
+++ src/fs.js
@@ -2,11 +2,50 @@ const Debug = require('debug');
 const Promise = require('bluebird');
 const fs = require('fs-extra');
 const path = require('path');
+const inquirer = require('inquirer');
 const keystore = require('./keystore');
 const { getChains } = require('./utils');
 
 const debug = Debug('iexec:utils');
+const openAsync = Promise.promisify(fs.open);
+const writeAsync = Promise.promisify(fs.write);
 const readFileAsync = Promise.promisify(fs.readFile);
+const writeFileAsync = Promise.promisify(fs.writeFile);
+
+const saveJSON = async (
+  fileName,
+  userWallet,
+  { force = false, message = 'overwrite?' } = {},
+) => {
+  const userJSONWallet = JSON.stringify(userWallet, null, 4);
+  try {
+    if (force) {
+      await writeFileAsync(fileName, userJSONWallet);
+      return fileName;
+    }
+    const fd = await openAsync(fileName, 'wx');
+    await writeAsync(fd, userJSONWallet, 0, 'utf8');
+    await fs.close(fd);
+    return fileName;
+  } catch (error) {
+    if (error.code === 'EEXIST') {
+      const answers = await inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'overwrite',
+          message,
+        },
+      ]);
+      if (answers.overwrite) {
+        await writeFileAsync(fileName, userJSONWallet);
+        return fileName;
+      }
+      throw Error('Aborted by user. keeping old wallet');
+    }
+    debug('save() error', error);
+    throw error;
+  }
+};
 
 const loadJSONFile = async (fileName) => {
   try {
@@ -51,6 +90,7 @@ const loadChain = async (chainName) => {
 };
 
 module.exports = {
+  saveJSON,
   loadIExecConf,
   loadChainsConf,
   loadChain,
