6ec0ec04393276629c43e5a3416ddd3c55d62b07
victor <victor@vbonhomme.fr>
2018-05-04 14:50:20 +0200

    refactor wallet

--- src/wallet.js
+++ src/wallet.js
@@ -2,10 +2,9 @@ const Debug = require('debug');
 const Promise = require('bluebird');
 const fetch = require('node-fetch');
 const inquirer = require('inquirer');
-const keystore = require('./keystore');
 const rlcJSON = require('rlc-faucet-contract/build/contracts/RLC.json');
-const { Spinner, handleError } = require('./cli-helper');
-const { getChains, getContractAddress, getRPCObjValue } = require('./utils');
+const { Spinner } = require('./cli-helper');
+const { getContractAddress, getRPCObjValue } = require('./utils');
 
 const debug = Debug('iexec:wallet');
 
@@ -50,30 +49,25 @@ const ethFaucets = [
   },
 ];
 
-const getETH = async (chainName) => {
+const getETH = async (chainName, account) => {
   const spinner = Spinner();
-  try {
-    const userWallet = await keystore.load();
+  spinner.start(`requesting ETH from ${chainName} faucets...`);
 
-    spinner.start(`Requesting ETH from ${chainName} faucets...`);
-    const filteredFaucets = ethFaucets.filter(e => e.chainName === chainName);
-    const responses = await Promise.all(filteredFaucets.map(faucet => faucet.getETH(userWallet.address)));
-    const responsesMessage = filteredFaucets.reduce(
-      (accu, curr, index) =>
-        accu.concat(
-          '- ',
-          curr.name,
-          ' : \n',
-          JSON.stringify(responses[index], null, '\t'),
-          '\n\n',
-        ),
-      '',
-    );
-    spinner.succeed('Faucets responses:\n');
-    console.log(responsesMessage);
-  } catch (error) {
-    handleError(error, 'wallet', spinner);
-  }
+  const filteredFaucets = ethFaucets.filter(e => e.chainName === chainName);
+  const responses = await Promise.all(filteredFaucets.map(faucet => faucet.getETH(account)));
+  const responsesString = filteredFaucets.reduce(
+    (accu, curr, index) =>
+      accu.concat(
+        '- ',
+        curr.name,
+        ' : \n',
+        JSON.stringify(responses[index], null, '\t'),
+        '\n\n',
+      ),
+    '',
+  );
+  spinner.succeed(`Faucets responses:\n${responsesString}`);
+  return responses;
 };
 
 const rlcFaucets = [
@@ -84,136 +78,116 @@ const rlcFaucets = [
   },
 ];
 
-const getRLC = async (chainName) => {
+const getRLC = async (chainName, account) => {
   const spinner = Spinner();
-  try {
-    const userWallet = await keystore.load();
 
-    spinner.start(`Requesting ${chainName} faucet for nRLC...`);
-    const responses = await Promise.all(rlcFaucets.map(faucet => faucet.getRLC(chainName, userWallet.address)));
-    const responsesMessage = rlcFaucets.reduce(
-      (accu, curr, index) =>
-        accu.concat(
-          '- ',
-          curr.name,
-          ' : \n',
-          JSON.stringify(responses[index], null, '\t'),
-          '\n\n',
-        ),
-      '',
-    );
-    spinner.succeed('Faucets responses:\n');
-    console.log(responsesMessage);
-  } catch (error) {
-    handleError(error, 'wallet', spinner);
-  }
+  spinner.start(`Requesting ${chainName} faucet for nRLC...`);
+  const responses = await Promise.all(rlcFaucets.map(faucet => faucet.getRLC(chainName, account)));
+  const responsesString = rlcFaucets.reduce(
+    (accu, curr, index) =>
+      accu.concat(
+        '- ',
+        curr.name,
+        ' : \n',
+        JSON.stringify(responses[index], null, '\t'),
+        '\n\n',
+      ),
+    '',
+  );
+  spinner.succeed(`Faucets responses:\n${responsesString}`);
+  return responses;
 };
 
-const sendETH = async (chainName, amount, to) => {
+const sendETH = async (chain, amount, to, account) => {
   const spinner = Spinner();
-  try {
-    const userWallet = await keystore.load();
-    const chain = getChains()[chainName];
 
-    const answers = await inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'transfer',
-        message: `Do you want to send ${amount} ${chainName} ETH to ${to} [chainID: ${
-          chain.id
-        }]`,
-      },
-    ]);
-    if (!answers.transfer) throw Error('Transfer aborted by user.');
+  const answers = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'transfer',
+      message: `Do you want to send ${amount} ${
+        chain.name
+      } ETH to ${to} [chainID: ${chain.id}]`,
+    },
+  ]);
+  if (!answers.transfer) throw Error('Transfer aborted by user.');
 
-    spinner.start(`Sending ${amount} ${chainName} ETH to ${to}...`);
+  spinner.start(`Sending ${amount} ${chain.name} ETH to ${to}...`);
 
-    const txHash = await chain.ethjs.sendTransaction({
-      from: userWallet.address,
-      data: '0x',
-      to,
-      value: chain.EthJS.toWei(amount, 'ether'),
-    });
-    spinner.info(`transfer txHash: ${txHash} \n`);
+  const txHash = await chain.ethjs.sendTransaction({
+    from: account,
+    data: '0x',
+    to,
+    value: chain.EthJS.toWei(amount, 'ether'),
+  });
+  spinner.info(`transfer txHash: ${txHash} \n`);
 
-    spinner.start('waiting for transaction to be mined');
-    const txReceipt = await chain.contracts.waitForReceipt(txHash);
+  spinner.start('waiting for transaction to be mined');
+  const txReceipt = await chain.contracts.waitForReceipt(txHash);
+  debug('txReceipt:', txReceipt);
 
-    debug('txReceipt:', JSON.stringify(txReceipt, null, 4));
-
-    spinner.succeed(`${amount} ${chainName} ETH sent to ${to}\n`);
-  } catch (error) {
-    handleError(error, 'wallet', spinner);
-  }
+  spinner.succeed(`${amount} ${chain.name} ETH sent from ${account} to ${to}\n`);
+  return txReceipt;
 };
 
-const sendRLC = async (chainName, amount, to, token) => {
+const sendRLC = async (chain, amount, to, token, account) => {
   const spinner = Spinner();
-  try {
-    const userWallet = await keystore.load();
-    const chain = getChains()[chainName];
 
-    const answers = await inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'transfer',
-        message: `Do you want to send ${amount} ${chainName} nRLC to ${to} [chainID: ${
-          chain.id
-        }]`,
-      },
-    ]);
-    if (!answers.transfer) throw Error('Transfer aborted by user.');
+  const answers = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'transfer',
+      message: `Do you want to send ${amount} ${
+        chain.name
+      } nRLC to ${to} [chainID: ${chain.id}]`,
+    },
+  ]);
+  if (!answers.transfer) throw Error('Transfer aborted by user.');
 
-    spinner.start(`Sending ${amount} ${chainName} nRLC to ${to}...`);
+  spinner.start(`Sending ${amount} ${chain.name} nRLC to ${to}...`);
 
-    const rlcAddress = token || getContractAddress(rlcJSON, chain.id);
-    const rlcContract = chain.ethjs.contract(rlcJSON.abi).at(rlcAddress);
+  const rlcAddress = token || getContractAddress(rlcJSON, chain.id);
+  const rlcContract = chain.ethjs.contract(rlcJSON.abi).at(rlcAddress);
 
-    const txHash = await rlcContract.transfer(to, amount, {
-      from: userWallet.address,
-      data: '0x',
-    });
-    spinner.info(`transfer txHash: ${txHash} \n`);
+  const txHash = await rlcContract.transfer(to, amount, {
+    from: account,
+    data: '0x',
+  });
+  spinner.info(`transfer txHash: ${txHash} \n`);
 
-    spinner.start('waiting for transaction to be mined');
-    const txReceipt = await chain.contracts.waitForReceipt(txHash);
+  spinner.start('waiting for transaction to be mined');
+  const txReceipt = await chain.contracts.waitForReceipt(txHash);
+  debug('txReceipt:', txReceipt);
 
-    debug('txReceipt:', JSON.stringify(txReceipt, null, 4));
+  const events = chain.contracts.decodeLogs(txReceipt.logs, rlcJSON.abi);
+  debug('events', events);
 
-    spinner.succeed(`${amount} ${chainName} nRLC sent to ${to}\n`);
-  } catch (error) {
-    handleError(error, 'wallet', spinner);
-  }
+  spinner.succeed(`${amount} ${chain.name} nRLC sent from ${account} to ${to}\n`);
+  return events;
 };
 
-const sweep = async (chainName, to, token) => {
+const sweep = async (chain, to, token, account) => {
   const spinner = Spinner();
-  try {
-    const userWallet = await keystore.load();
 
-    const chain = getChains()[chainName];
+  const rlcAddress = token || getContractAddress(rlcJSON, chain.id);
+  const rlcContract = chain.ethjs.contract(rlcJSON.abi).at(rlcAddress);
 
-    const rlcAddress = token || getContractAddress(rlcJSON, chain.id);
-    const rlcContract = chain.ethjs.contract(rlcJSON.abi).at(rlcAddress);
-
-    const rlcBalanceRPC = await rlcContract.balanceOf(userWallet.address);
-    const rlcBalance = getRPCObjValue(rlcBalanceRPC);
-    debug('rlcBalance', rlcBalance.toNumber());
-    if (rlcBalance.toNumber() > 0) {
-      await sendRLC(chainName, rlcBalance, to, token);
-    }
-
-    const ethBalance = await chain.ethjs
-      .getBalance(userWallet.address)
-      .then(balance => chain.EthJS.fromWei(balance, 'ether'));
-    debug('ethBalance', ethBalance);
-    const ethToSweep = ethBalance - 0.01;
-    if (ethToSweep > 0) await sendETH(chainName, ethToSweep, to);
-
-    spinner.succeed(`wallet swept to ${to}\n`);
-  } catch (error) {
-    handleError(error, 'wallet', spinner);
+  const rlcBalanceRPC = await rlcContract.balanceOf(account);
+  const rlcBalance = getRPCObjValue(rlcBalanceRPC);
+  debug('rlcBalance', rlcBalance.toNumber());
+  if (rlcBalance.toNumber() > 0) {
+    await sendRLC(chain.name, rlcBalance, to, token);
   }
+
+  const ethBalance = await chain.ethjs
+    .getBalance(account)
+    .then(balance => chain.EthJS.fromWei(balance, 'ether'));
+  debug('ethBalance', ethBalance);
+  const ethToSweep = ethBalance - 0.01;
+  if (ethToSweep > 0) await sendETH(chain.name, ethToSweep, to);
+
+  spinner.succeed(`wallet swept from ${account} to ${to}\n`);
+  return true;
 };
 
 module.exports = {
