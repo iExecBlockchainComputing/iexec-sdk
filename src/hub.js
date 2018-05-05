f186dceab8bd18fbc7ab2eb39b6ce47d244379c5
victor <victor@vbonhomme.fr>
2018-05-05 15:06:15 +0200

    agree on api

--- src/hub.js
+++ src/hub.js
@@ -1,16 +1,15 @@
 const Debug = require('debug');
 const ethUtil = require('ethjs-util');
 const { isEthAddress } = require('./utils');
-const { Spinner } = require('./cli-helper');
+const { Spinner, info } = require('./cli-helper');
 
 const debug = Debug('iexec:hub');
 
-const createObj = objName => async (hubAddress, obj, contracts) => {
+const createObj = objName => async (contracts, obj, options) => {
   const spinner = Spinner();
-  spinner.start(`creating ${objName}...`);
-  const txHash = await contracts.createObj(objName)(obj, {
-    hub: hubAddress,
-  });
+  spinner.start(info.creating(objName));
+
+  const txHash = await contracts.createObj(objName)(obj, options);
 
   const txReceipt = await contracts.waitForReceipt(txHash);
   debug('txReceipt', txReceipt);
@@ -23,13 +22,14 @@ const createObj = objName => async (hubAddress, obj, contracts) => {
 };
 
 const showObj = objName => async (
-  objAdressOrIndex,
-  cliHubAddress,
-  userAddress,
   contracts,
+  objAdressOrIndex,
+  userAddress,
+  options,
 ) => {
   const spinner = Spinner();
-  spinner.start(`showing ${objName}...`);
+  spinner.start(info.showing(objName));
+
   let objAddress;
   if (
     !ethUtil.isHexString(objAdressOrIndex) &&
@@ -39,7 +39,7 @@ const showObj = objName => async (
     objAddress = await contracts.getUserObjAddressByIndex(objName)(
       userAddress,
       objAdressOrIndex,
-      { hub: cliHubAddress },
+      options,
     );
   } else if (isEthAddress(objAdressOrIndex)) {
     objAddress = objAdressOrIndex;
@@ -47,37 +47,30 @@ const showObj = objName => async (
     throw Error('argument is neither an integer index nor a valid ethereum address');
   }
 
-  const obj = await contracts.getObjProps(objName)(objAddress, {
-    hub: cliHubAddress,
-  });
+  const obj = await contracts.getObjProps(objName)(objAddress);
 
   spinner.succeed(`${objName} ${objAddress} details:\n${JSON.stringify(obj, null, 4)}`);
   return obj;
 };
 
-const countObj = objName => async (
-  cliUserAddress,
-  hubAddress,
-  userAddress,
-  contracts,
-) => {
+const countObj = objName => async (contracts, userAddress, options) => {
   const spinner = Spinner();
-  spinner.start(`counting ${objName}...`);
+  spinner.start(info.counting(objName));
 
-  const objCount = await contracts.getUserObjCount(objName)(userAddress, {
-    hub: hubAddress,
-  });
+  const objCount = await contracts.getUserObjCount(objName)(
+    userAddress,
+    options,
+  );
 
   spinner.succeed(`user ${userAddress} has a total of ${objCount} ${objName}`);
   return objCount;
 };
 
-const createCategory = async (hubAddress, obj, contracts) => {
+const createCategory = async (contracts, obj, options) => {
   const spinner = Spinner();
-  spinner.start('creating category...');
-  const txHash = await contracts.createCategory(obj, {
-    hub: hubAddress,
-  });
+  spinner.start(info.creating('category'));
+
+  const txHash = await contracts.createCategory(obj, options);
 
   const txReceipt = await contracts.waitForReceipt(txHash);
   debug('txReceipt', txReceipt);
@@ -89,31 +82,23 @@ const createCategory = async (hubAddress, obj, contracts) => {
   return events;
 };
 
-const showCategory = async (index, hubAddress, contracts, objName) => {
+const showCategory = async (contracts, index, options) => {
   const spinner = Spinner();
-  spinner.start(`showing ${objName}...`);
+  spinner.start(info.showing('category'));
 
-  const category = await contracts.getCategoryByIndex(index, {
-    hub: hubAddress,
-  });
+  const category = await contracts.getCategoryByIndex(index, options);
 
-  spinner.succeed(`${objName} at index ${index} details:\n${JSON.stringify(
-    category,
-    null,
-    4,
-  )}`);
+  spinner.succeed(`category at index ${index} details:\n${JSON.stringify(category, null, 4)}`);
   return category;
 };
 
-const countCategory = async (hubAddress, contracts, objName) => {
+const countCategory = async (contracts, options) => {
   const spinner = Spinner();
-  spinner.start(`counting ${objName}...`);
+  spinner.start(info.counting('category'));
 
-  const count = await contracts.getHubCategoryCount({
-    address: hubAddress,
-  });
+  const count = await contracts.getHubCategoryCount(options);
 
-  spinner.succeed(`iExec hub has a total of ${count} ${objName}`);
+  spinner.succeed(`iExec hub has a total of ${count} category`);
   return count;
 };
 
