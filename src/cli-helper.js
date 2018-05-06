eebc5fcc50e6a6d1a27c5be010412157775684da
victor <victor@vbonhomme.fr>
2018-05-06 11:34:45 +0200

    add cli helpers

--- src/cli-helper.js
+++ src/cli-helper.js
@@ -2,6 +2,7 @@ const Debug = require('debug');
 const colors = require('colors/safe');
 const Ora = require('ora');
 const inquirer = require('inquirer');
+const prettyjson = require('prettyjson');
 
 const debug = Debug('help');
 
@@ -9,7 +10,7 @@ const info = {
   waitMiners: () => 'waiting for transaction to be mined...',
   checkBalance: currency => `checking ${currency} balances...`,
   topUp: currency =>
-    `Run "iexec wallet get${currency}" to top up your ${currency} account`,
+    `Run "iexec wallet get${currency}" to ask faucets for ${currency}`,
   userAborted: () => 'operation aborted by user.',
   logging: () => 'logging into iExec...',
   creating: obj => `creating ${obj}...`,
@@ -67,10 +68,18 @@ const question = async (message, error = 'operation aborted by user') => {
 };
 
 const prompt = {
+  custom: question,
   create: file => question(`You don't have a ${file} yet, create one?`),
   overwrite: file =>
     question(`${file} already exists, replace it with new one?`),
+  transfer: (currency, amount, chainName, to, chainID) =>
+    question(`Do you want to send ${amount} ${chainName} ${currency} to ${to} [chainID: ${chainID}]`),
 };
+
+prompt.transferETH = (...args) => prompt.transfer('ETH', ...args);
+prompt.transferRLC = (...args) => prompt.transfer('nRLC', ...args);
+prompt.sweep = (...args) =>
+  prompt.transfer('ETH and RLC', 'all wallet', ...args);
 
 const helpMessage = () => {
   console.log('');
@@ -176,6 +185,23 @@ const handleError = (error, anchorName, spinner = Spinner()) => {
   process.exit(1);
 };
 
+const lbb = (str = '') => `\n${str}`;
+const lba = (str = '') => `${str}\n`;
+const lb = str => lba(lbb(str));
+
+const pretty = (obj, options) => lb(prettyjson.render(obj, options));
+
+const prettyRPC = (rpcObj) => {
+  const keys = Object.keys(rpcObj);
+  const prettyObj = keys.reduce((accu, curr) => {
+    if (Number.isNaN(parseInt(curr, 10))) {
+      return Object.assign(accu, { [curr]: rpcObj[curr].toString() });
+    }
+    return accu;
+  }, {});
+  return pretty(prettyObj);
+};
+
 module.exports = {
   help,
   Spinner,
@@ -185,4 +211,9 @@ module.exports = {
   desc,
   option,
   prompt,
+  pretty,
+  prettyRPC,
+  lbb,
+  lba,
+  lb,
 };
