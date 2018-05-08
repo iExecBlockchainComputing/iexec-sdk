825ed7c9d8a312de10ebb15952689fbceab8fe76
victor <victor@vbonhomme.fr>
2018-05-08 15:41:58 +0200

    update tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,48 +1,51 @@
-const fs = require('fs-extra');
+// const fs = require('fs-extra');
 const { exec } = require('child_process');
 const Promise = require('bluebird');
 
 const execAsync = Promise.promisify(exec);
 
-test(
-  'iexec init',
-  () => expect(execAsync('iexec init')).resolves.not.toBe(1),
-  10000,
-);
+test('iexec init', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js init --force')).resolves.not.toBe(1));
 
-test('iexec wallet create', () => {
-  process.chdir('iexec-init');
-  return expect(execAsync('iexec wallet create')).resolves.not.toBe(1);
-});
+test('iexec wallet create', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js wallet create --force')).resolves.not.toBe(1));
 
 test(
   'iexec wallet show',
-  () => expect(execAsync('iexec wallet show')).resolves.not.toBe(1),
+  () =>
+    expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js wallet show')).resolves.not.toBe(1),
   10000,
 );
 
 test('iexec account login', () =>
-  expect(execAsync('iexec account login')).resolves.not.toBe(1));
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js account login --force')).resolves.not.toBe(1));
 
 test(
   'iexec account show',
-  () => expect(execAsync('iexec account show')).resolves.not.toBe(1),
+  () =>
+    expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js account show --chain kovan')).resolves.not.toBe(1),
   10000,
 );
 
-test(
-  'iexec compile',
-  () => expect(execAsync('iexec compile')).resolves.not.toBe(1),
-  20000,
-);
+test('iexec app init', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js app init')).resolves.not.toBe(1));
 
-test('iexec truffle version', () =>
-  expect(execAsync('iexec truffle version')).resolves.not.toBe(1));
+test('iexec dataset init', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js dataset init')).resolves.not.toBe(1));
 
-test('iexec server version', () =>
-  expect(execAsync('iexec server version')).resolves.not.toBe(1));
+test('iexec workerpool init', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js workerpool init')).resolves.not.toBe(1));
+
+test('iexec category init', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js category init')).resolves.not.toBe(1));
+
+test('iexec order init --sell', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js order init --sell')).resolves.not.toBe(1));
+
+test('iexec order init --buy', () =>
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js order init --buy')).resolves.not.toBe(1));
 
 test('iexec upgrade', () =>
-  expect(execAsync('iexec upgrade')).resolves.not.toBe(1));
+  expect(execAsync('node /home/victor/code/github.com/iExecBlockchainComputing/iexec-sdk/src/iexec.js upgrade')).resolves.not.toBe(1));
 
-afterAll(() => fs.remove('iexec-init'));
+afterAll(() => console.log('finished'));
