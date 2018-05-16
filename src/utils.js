8f30bc01cc1f3a6ca7ebc349a3536e07649fa9e5
victor <victor@vbonhomme.fr>
2018-05-16 11:50:27 +0200

    clean utils

--- src/utils.js
+++ src/utils.js
@@ -4,54 +4,6 @@ const jws = require('jws');
 
 const debug = Debug('iexec:utils');
 
-const FETCH_INTERVAL = 1000;
-const TIMEOUT = 60 * 1000;
-const sleep = ms => new Promise(res => setTimeout(res, ms));
-
-const waitFor = async (fn, hash) => {
-  let counter = 0;
-  try {
-    const txReceipt = await fn(hash);
-    if (counter < TIMEOUT && txReceipt === null) {
-      await sleep(FETCH_INTERVAL);
-      counter += FETCH_INTERVAL;
-      return waitFor(fn, hash);
-    } else if (counter > TIMEOUT) {
-      throw Error('TIMEOUT: Transaction still not included in a Block');
-    }
-    return txReceipt;
-  } catch (error) {
-    debug('waitFor()', error);
-    throw error;
-  }
-};
-
-const chainToEtherscanURL = chainName =>
-  `https://${
-    chainName === 'mainnet' ? '' : chainName.concat('.')
-  }etherscan.io/tx/`;
-
-const ORACLE_WALLET_ADDRESS = '0x486a5986f795d323555c0321d655f1eb78d68381';
-const getOracleWallet = (to) => {
-  const toAddress = to === 'iexec' ? ORACLE_WALLET_ADDRESS : to;
-  return toAddress;
-};
-
-const FAUCET_WALLET_ADDRESS = '0x1d78323c836d6e6681fe77128ae55923c8d5e0f0';
-const getFaucetWallet = (to) => {
-  const toAddress = to === 'iexec' ? FAUCET_WALLET_ADDRESS : to;
-  return toAddress;
-};
-
-const checkTxReceipt = (txReceipt, gasLimit) => {
-  if (txReceipt.status === '0x0') {
-    throw Error('transaction failed, state REVERTED');
-  }
-  if (txReceipt.gasUsed === gasLimit) {
-    throw Error('transaction throw, out of gas');
-  }
-};
-
 const getContractAddress = (desc, chainID, { strict = true } = {}) => {
   try {
     if (!('networks' in desc)) {
@@ -75,32 +27,6 @@ const getContractAddress = (desc, chainID, { strict = true } = {}) => {
     return desc.networks[chainID].address;
   } catch (error) {
     debug('getContractAddress()', error);
-    throw error;
-  }
-};
-
-const getRPCObjValue = (obj, { strict = true } = {}) => {
-  if (!(typeof obj === 'object')) {
-    if (strict) throw Error('RPC object is not of type object');
-    return obj;
-  }
-  if (!('0' in obj)) {
-    if (strict) throw Error('no "0" key in RPC response object');
-    return undefined;
-  }
-  return obj[0];
-};
-
-const stringifyRPCObj = (obj) => {
-  try {
-    const str = Object.keys(obj).reduce(
-      (accu, curr) =>
-        accu.concat(`\n   ${curr}: ${getRPCObjValue(obj[curr], { strict: false })}`),
-      '   ',
-    );
-    return str;
-  } catch (error) {
-    debug('stringifyRPCObj()', error);
     throw error;
   }
 };
@@ -134,15 +60,8 @@ const decodeJWTForPrint = (jwtoken) => {
 const decodeJWT = jws.decode;
 
 module.exports = {
-  waitFor,
-  chainToEtherscanURL,
-  getOracleWallet,
-  getFaucetWallet,
-  checkTxReceipt,
   getContractAddress,
-  stringifyRPCObj,
   isEthAddress,
-  getRPCObjValue,
   toUpperFirst,
   secToDate,
   decodeJWTForPrint,
