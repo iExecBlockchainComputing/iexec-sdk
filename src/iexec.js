6abec4bc7084e339ae25f0339237dac7f1eee7d7
victor <victor@vbonhomme.fr>
2018-06-04 21:48:14 +0200

    move update check to cli top level

--- src/iexec.js
+++ src/iexec.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const cli = require('commander');
+const Debug = require('debug');
+const checkForUpdate = require('update-check');
+const isDocker = require('is-docker');
 const {
   handleError,
   help,
@@ -17,76 +20,94 @@ const { decodeJWTForPrint } = require('./utils');
 const packageJSON = require('../package.json');
 
 cli.description(packageJSON.description).version(packageJSON.version);
+const debug = Debug('iexec');
+const NODEJS_UPGRADE_CMD = 'npm -g i iexec';
+const DOCKER_UPGRADE_CMD = 'docker pull iexechub/iexec-sdk';
 
-cli
-  .command('init')
-  .option(...option.force())
-  .description(desc.initObj('project'))
-  .action(async (cmd) => {
-    const spinner = Spinner();
-    try {
-      const { force } = cmd;
-      const { saved, fileName } = await initIExecConf({ force, strict: false });
-      if (fileName) {
-        spinner.info(`Here is your main config "${fileName}":${pretty(saved)}`);
+async function main() {
+  const update = await checkForUpdate(packageJSON, { interval: 10 }).catch(debug);
+
+  if (update) {
+    const upgradeCMD = isDocker() ? DOCKER_UPGRADE_CMD : NODEJS_UPGRADE_CMD;
+    const spin = Spinner();
+    spin.info(`iExec SDK update available ${packageJSON.version} →  ${
+      update.latest
+    }, Run "${upgradeCMD}" to update\n`);
+  }
+
+  cli
+    .command('init')
+    .option(...option.force())
+    .description(desc.initObj('project'))
+    .action(async (cmd) => {
+      const spinner = Spinner();
+      try {
+        const { force } = cmd;
+        const { saved, fileName } = await initIExecConf({
+          force,
+          strict: false,
+        });
+        if (fileName) {
+          spinner.info(`Here is your main config "${fileName}":${pretty(saved)}`);
+        }
+
+        const chainRes = await initChainConf({ force, strict: false });
+        if (chainRes.fileName) {
+          spinner.info(`Here is your chain config "${chainRes.fileName}":${pretty(chainRes.saved)}`);
+        }
+
+        const walletRes = await keystore.createAndSave({
+          force,
+          strict: false,
+        });
+        if (walletRes.fileName) {
+          spinner.info(`wallet saved in "${walletRes.fileName}":\n${pretty(walletRes.wallet)}`);
+        }
+
+        const [chain, keys] = await Promise.all([
+          loadChain('ropsten'),
+          keystore.load({ lowercase: true }),
+        ]);
+        const jwtoken = await account.auth(
+          keys.address,
+          chain.iexec,
+          chain.ethjs,
+        );
+
+        const accountfileName = await saveAccountConf(
+          { jwtoken },
+          { force: true },
+        );
+        const jwtForPrint = decodeJWTForPrint(jwtoken);
+        spinner.succeed(`You are logged into iExec. Login token saved into "${accountfileName}":${pretty(jwtForPrint)}`);
+
+        spinner.succeed('iExec project is ready\n');
+      } catch (error) {
+        handleError(error, cli);
       }
+    });
 
-      const chainRes = await initChainConf({ force, strict: false });
-      if (chainRes.fileName) {
-        spinner.info(`Here is your chain config "${chainRes.fileName}":${pretty(chainRes.saved)}`);
-      }
+  cli.command('wallet', 'manage local ethereum wallet');
 
-      const walletRes = await keystore.createAndSave({
-        force,
-        strict: false,
-      });
-      if (walletRes.fileName) {
-        spinner.info(`wallet saved in "${walletRes.fileName}":\n${pretty(walletRes.wallet)}`);
-      }
+  cli.command('account', 'manage iExec account');
 
-      const [chain, keys] = await Promise.all([
-        loadChain('ropsten'),
-        keystore.load({ lowercase: true }),
-      ]);
-      const jwtoken = await account.auth(
-        keys.address,
-        chain.iexec,
-        chain.ethjs,
-      );
+  cli.command('scheduler', 'interact with an iExec scheduler');
 
-      const accountfileName = await saveAccountConf(
-        { jwtoken },
-        { force: true },
-      );
-      const jwtForPrint = decodeJWTForPrint(jwtoken);
-      spinner.succeed(`You are logged into iExec. Login token saved into "${accountfileName}":${pretty(jwtForPrint)}`);
+  cli.command('app', 'manage iExec apps');
 
-      spinner.succeed('iExec project is ready\n');
-    } catch (error) {
-      handleError(error, cli);
-    }
-  });
+  cli.command('dataset', 'manage iExec datasets');
 
-cli.command('wallet', 'manage local ethereum wallet');
+  cli.command('category', 'manage iExec categories');
 
-cli.command('account', 'manage iExec account');
+  cli.command('workerpool', 'manage iExec workerpools');
 
-cli.command('scheduler', 'interact with an iExec scheduler');
+  cli.command('orderbook', 'show marketplace orderbook');
 
-cli.command('upgrade', 'update iExec CLI tool and upgrade iExec project');
+  cli.command('order', 'manage iExec marketplace orders');
 
-cli.command('app', 'manage iExec apps');
+  cli.command('work', 'manage iExec works');
 
-cli.command('dataset', 'manage iExec datasets');
+  help(cli);
+}
 
-cli.command('category', 'manage iExec categories');
-
-cli.command('workerpool', 'manage iExec workerpools');
-
-cli.command('orderbook', 'show marketplace orderbook');
-
-cli.command('order', 'manage iExec marketplace orders');
-
-cli.command('work', 'manage iExec works');
-
-help(cli);
+main();
