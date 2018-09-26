f0b3b0e2c22d1c710ed50b9376faa00ed4fb23f7
victor <victor@vbonhomme.fr>
2018-09-26 09:34:21 +0200

    lint

--- test/cli.test.js
+++ test/cli.test.js
@@ -11,20 +11,21 @@ test('iexec init', () => {
   return expect(execAsync(`${iexecPath} init --force`)).resolves.not.toBe(1);
 });
 
-test('iexec wallet create', () =>
-  expect(execAsync(`${iexecPath} wallet create --force`)).resolves.not.toBe(1));
+test('iexec wallet create', () => expect(execAsync(`${iexecPath} wallet create --force`)).resolves.not.toBe(1));
 
 test(
   'iexec wallet encrypt',
-  () =>
-    expect(execAsync(`${iexecPath} wallet encrypt --password toto --force`)).resolves.not.toBe(1),
+  () => expect(
+    execAsync(`${iexecPath} wallet encrypt --password toto --force`),
+  ).resolves.not.toBe(1),
   10000,
 );
 
 test(
   'iexec wallet decrypt',
-  () =>
-    expect(execAsync(`${iexecPath} wallet decrypt --password toto --force`)).resolves.not.toBe(1),
+  () => expect(
+    execAsync(`${iexecPath} wallet decrypt --password toto --force`),
+  ).resolves.not.toBe(1),
   10000,
 );
 
@@ -34,8 +35,7 @@ test(
   10000,
 );
 
-test('iexec account login', () =>
-  expect(execAsync(`${iexecPath} account login --force`)).resolves.not.toBe(1));
+test('iexec account login', () => expect(execAsync(`${iexecPath} account login --force`)).resolves.not.toBe(1));
 
 test(
   'iexec account show',
@@ -43,32 +43,22 @@ test(
   10000,
 );
 
-test('iexec app init', () =>
-  expect(execAsync(`${iexecPath} app init`)).resolves.not.toBe(1));
+test('iexec app init', () => expect(execAsync(`${iexecPath} app init`)).resolves.not.toBe(1));
 
-test('iexec app count', () =>
-  expect(execAsync(`${iexecPath} app count`)).resolves.not.toBe(1));
+test('iexec app count', () => expect(execAsync(`${iexecPath} app count`)).resolves.not.toBe(1));
 
-test('iexec dataset init', () =>
-  expect(execAsync(`${iexecPath} dataset init`)).resolves.not.toBe(1));
+test('iexec dataset init', () => expect(execAsync(`${iexecPath} dataset init`)).resolves.not.toBe(1));
 
-test('iexec workerpool init', () =>
-  expect(execAsync(`${iexecPath} workerpool init`)).resolves.not.toBe(1));
+test('iexec workerpool init', () => expect(execAsync(`${iexecPath} workerpool init`)).resolves.not.toBe(1));
 
-test('iexec category init', () =>
-  expect(execAsync(`${iexecPath} category init`)).resolves.not.toBe(1));
+test('iexec category init', () => expect(execAsync(`${iexecPath} category init`)).resolves.not.toBe(1));
 
-test('iexec order init --sell', () =>
-  expect(execAsync(`${iexecPath} order init --sell`)).resolves.not.toBe(1));
+test('iexec order init --sell', () => expect(execAsync(`${iexecPath} order init --sell`)).resolves.not.toBe(1));
 
-test('iexec order init --buy', () =>
-  expect(execAsync(`${iexecPath} order init --buy`)).resolves.not.toBe(1));
+test('iexec order init --buy', () => expect(execAsync(`${iexecPath} order init --buy`)).resolves.not.toBe(1));
 
-test('iexec order count', () =>
-  expect(execAsync(`${iexecPath} order count`)).resolves.not.toBe(1));
+test('iexec order count', () => expect(execAsync(`${iexecPath} order count`)).resolves.not.toBe(1));
 
-test('iexec workerpool count', () =>
-  expect(execAsync(`${iexecPath} app count`)).resolves.not.toBe(1));
+test('iexec workerpool count', () => expect(execAsync(`${iexecPath} app count`)).resolves.not.toBe(1));
 
-test('iexec orderbook show', () =>
-  expect(execAsync(`${iexecPath} orderbook show`)).resolves.not.toBe(1));
+test('iexec orderbook show', () => expect(execAsync(`${iexecPath} orderbook show`)).resolves.not.toBe(1));
