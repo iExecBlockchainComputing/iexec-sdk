dbfa25a4ecc1b0d3da0778e9a2f7b0084fba6f61
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-01-07 10:20:37 +0100

    add --raw (orderbook)

--- src/iexec-orderbook.js
+++ src/iexec-orderbook.js
@@ -4,6 +4,7 @@ const Debug = require('debug');
 const cli = require('commander');
 const {
   help,
+  addGlobalOptions,
   handleError,
   desc,
   option,
@@ -18,16 +19,19 @@ const { http } = require('./utils');
 const debug = Debug('iexec:iexec-orderbook');
 const objName = 'orderbook';
 
-cli
-  .command(command.show())
+const show = cli.command(command.show());
+addGlobalOptions(show);
+show
   .option(...option.chain())
   .option(...option.category())
   .option(...option.workerpool())
   .description(desc.showObj(objName, 'marketplace'))
   .action(async (cmd) => {
-    const spinner = Spinner();
+    const spinner = Spinner(cmd);
     try {
-      const chain = await loadChain(cmd.chain);
+      if (cmd.category === undefined) throw new Error('Missing option --category');
+
+      const chain = await loadChain(cmd.chain, spinner);
       debug('cmd.id', cmd.id);
       debug('cmd.category', cmd.category);
       debug('cmd.workerpool', cmd.workerpool);
@@ -40,31 +44,41 @@ cli
       debug('response', response);
       const workerpoolOrders = response.orderbook.workerpoolOrders.map(e => ({
         orderHash: e.orderHash,
-        category: e.category,
-        price: e.workerpoolprice,
-        remainingVolume: e.remain,
-        workerpool: e.workerpool,
+        category: e.order.category,
+        price: e.order.workerpoolprice,
+        remaining: e.remaining,
+        workerpool: e.order.workerpool,
         publicationTimestamp: e.publicationTimestamp,
       }));
       const requestOrders = response.orderbook.requestOrders.map(e => ({
         orderHash: e.orderHash,
-        category: e.category,
-        price: e.workerpoolmaxprice,
-        remainingVolume: e.remain,
-        workerpool: e.workerpool,
+        category: e.order.category,
+        price: e.order.workerpoolmaxprice,
+        remaining: e.remaining,
         publicationTimestamp: e.publicationTimestamp,
       }));
+      let successMessage;
       if (workerpoolOrders.length !== 0 || requestOrders.length !== 0) {
+        successMessage = 'Orderbook\n';
         if (workerpoolOrders.length !== 0) {
-          spinner.succeed(
-            `workerpool orders details:${pretty(workerpoolOrders)}`,
-          );
+          successMessage += `workerpool orders details:${pretty(
+            workerpoolOrders,
+          )}\n`;
         }
-        if (requestOrders.length !== 0) spinner.succeed(`request orders details:${pretty(requestOrders)}`);
-      } else spinner.succeed('empty order book');
+        if (requestOrders.length !== 0) {
+          successMessage += `request orders details:${pretty(requestOrders)}\n`;
+        }
+      } else successMessage = 'Empty order book';
+      debug('successMessage', successMessage);
+      spinner.succeed(successMessage, {
+        raw: {
+          workerpoolOrders: response.orderbook.workerpoolOrders,
+          requestOrders: response.orderbook.requestOrders,
+        },
+      });
       spinner.info('trade in the browser at https://market.iex.ec');
     } catch (error) {
-      handleError(error, cli);
+      handleError(error, cli, cmd);
     }
   });
 
