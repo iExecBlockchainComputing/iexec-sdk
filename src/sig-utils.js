280ee94bca5f1a3780c80c63b0130d3959af1f14
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-03-08 17:29:28 +0100

    clean sig-utils

--- src/sig-utils.js
+++ src/sig-utils.js
@@ -2,170 +2,23 @@ const Debug = require('debug');
 const ethjsUtil = require('ethjs-util');
 const EC = require('elliptic').ec;
 const BN = require('bn.js');
-const keccak256 = require('js-sha3').keccak_256;
-const ethers = require('ethers');
+const {
+  defaultAbiCoder,
+  keccak256,
+  solidityKeccak256,
+  bigNumberify,
+} = require('ethers').utils;
 
 const debug = Debug('iexec:sig-utils');
 const secp256k1 = new EC('secp256k1');
 
-const zeros = bytes => Buffer.allocUnsafe(bytes).fill(0);
-
-const setLength = (_msg, length, right) => {
-  const buf = zeros(length);
-
-  const msg = Buffer.from(_msg);
-  if (right) {
-    if (msg.length < length) {
-      msg.copy(buf);
-      return buf;
-    }
-    return msg.slice(0, length);
-  }
-  if (msg.length < length) {
-    msg.copy(buf, length - msg.length);
-    return buf;
-  }
-  return msg.slice(-length);
-};
-
-function addHexPrefix(str) {
+const addHexPrefix = (str) => {
   if (typeof str !== 'string') {
     return str;
   }
-
   return ethjsUtil.isHexPrefixed(str) ? str : `0x${str}`;
-}
-
-// Convert from short to canonical names
-// FIXME: optimise or make this nicer?
-function elementaryName(name) {
-  if (name.startsWith('int[')) {
-    return `int256${name.slice(3)}`;
-  }
-  if (name === 'int') {
-    return 'int256';
-  }
-  if (name.startsWith('uint[')) {
-    return `uint256${name.slice(4)}`;
-  }
-  if (name === 'uint') {
-    return 'uint256';
-  }
-  if (name.startsWith('fixed[')) {
-    return `fixed128x128${name.slice(5)}`;
-  }
-  if (name === 'fixed') {
-    return 'fixed128x128';
-  }
-  if (name.startsWith('ufixed[')) {
-    return `ufixed128x128${name.slice(6)}`;
-  }
-  if (name === 'ufixed') {
-    return 'ufixed128x128';
-  }
-  return name;
-}
-
-// Parse N from type<N>
-function parseTypeN(type) {
-  return parseInt(/^\D+(\d+)$/.exec(type)[1], 10);
-}
-
-function parseNumber(arg) {
-  const type = typeof arg;
-  if (type === 'string') {
-    if (ethjsUtil.isHexPrefixed(arg)) {
-      return new BN(ethjsUtil.stripHexPrefix(arg), 16);
-    }
-    return new BN(arg, 10);
-  }
-  if (type === 'number') {
-    return new BN(arg);
-  }
-  if (arg.toArray) {
-    // assume this is a BN for the moment, replace with BN.isBN soon
-    return arg;
-  }
-  throw new Error('Argument is not a number');
-}
-
-const solidityPack = (types, values) => {
-  if (types.length !== values.length) {
-    throw new Error('Number of types are not matching the values');
-  }
-
-  let size;
-  let num;
-  const ret = [];
-
-  for (let i = 0; i < types.length; i += 1) {
-    const type = elementaryName(types[i]);
-    const value = values[i];
-
-    if (type === 'bytes') {
-      ret.push(value);
-    } else if (type === 'string') {
-      ret.push(Buffer.from(value, 'utf8'));
-    } else if (type === 'bool') {
-      ret.push(Buffer.from(value ? '01' : '00', 'hex'));
-    } else if (type === 'address') {
-      ret.push(setLength(value, 20));
-    } else if (type.startsWith('bytes')) {
-      size = parseTypeN(type);
-      if (size < 1 || size > 32) {
-        throw new Error(`Invalid bytes<N> width: ${size}`);
-      }
-
-      ret.push(setLength(value, size, true));
-    } else if (type.startsWith('uint')) {
-      size = parseTypeN(type);
-      if (size % 8 || size < 8 || size > 256) {
-        throw new Error(`Invalid uint<N> width: ${size}`);
-      }
-
-      num = parseNumber(value);
-      if (num.bitLength() > size) {
-        throw new Error(
-          `Supplied uint exceeds width: ${size} vs ${num.bitLength()}`,
-        );
-      }
-
-      ret.push(num.toArrayLike(Buffer, 'be', size / 8));
-    } else if (type.startsWith('int')) {
-      size = parseTypeN(type);
-      if (size % 8 || size < 8 || size > 256) {
-        throw new Error(`Invalid int<N> width: ${size}`);
-      }
-
-      num = parseNumber(value);
-      if (num.bitLength() > size) {
-        throw new Error(
-          `Supplied int exceeds width: ${size} vs ${num.bitLength()}`,
-        );
-      }
-
-      ret.push(num.toTwos(size).toArrayLike(Buffer, 'be', size / 8));
-    } else {
-      // FIXME: support all other types
-      throw new Error(`Unsupported or invalid type: ${type}`);
-    }
-  }
-
-  return Buffer.concat(ret);
 };
 
-const soliditySHA3 = (types, values) => {
-  const solPack = solidityPack(types, values);
-  const solSHA3 = Buffer.from(keccak256(solPack), 'hex');
-  return solSHA3;
-};
-
-const ethersKeccak256 = bufferOrHexString => ethers.utils.keccak256(bufferOrHexString);
-
-/**
- * @param typedData - Array of data along with types, as per EIP712.
- * @returns Buffer
- */
 const typedSignatureHash = (typedData) => {
   const error = new Error('Expect argument to be non-empty array');
   if (typeof typedData !== 'object' || !typedData.length) throw error;
@@ -177,11 +30,11 @@ const typedSignatureHash = (typedData) => {
     return `${e.type} ${e.name}`;
   });
 
-  return soliditySHA3(
+  return solidityKeccak256(
     ['bytes32', 'bytes32'],
     [
-      soliditySHA3(new Array(typedData.length).fill('string'), schema),
-      soliditySHA3(types, data),
+      solidityKeccak256(new Array(typedData.length).fill('string'), schema),
+      solidityKeccak256(types, data),
     ],
   );
 };
@@ -266,7 +119,7 @@ const getStructType = (primaryType, members) => {
 
 const hashStruct = (primaryType, members, obj) => {
   const type = getStructType(primaryType, members);
-  const typeHash = ethersKeccak256(Buffer.from(type, 'utf8'));
+  const typeHash = keccak256(Buffer.from(type, 'utf8'));
   const encodedTypes = ['bytes32'].concat(
     members.map((e) => {
       if (e.type === 'string' || e.type === 'bytes') return 'bytes32';
@@ -275,14 +128,14 @@ const hashStruct = (primaryType, members, obj) => {
   );
   const values = [typeHash].concat(
     members.map((e) => {
-      if (e.type === 'string') return ethersKeccak256(Buffer.from(obj[e.name], 'utf8'));
-      if (e.type === 'uint256') return ethers.utils.bigNumberify(obj[e.name]);
-      if (e.type === 'uint8') return ethers.utils.bigNumberify(obj[e.name]);
+      if (e.type === 'string') return keccak256(Buffer.from(obj[e.name], 'utf8'));
+      if (e.type === 'uint256') return bigNumberify(obj[e.name]);
+      if (e.type === 'uint8') return bigNumberify(obj[e.name]);
       return obj[e.name];
     }),
   );
-  const encoded = ethers.utils.defaultAbiCoder.encode(encodedTypes, values);
-  const structHash = ethersKeccak256(encoded);
+  const encoded = defaultAbiCoder.encode(encodedTypes, values);
+  const structHash = keccak256(encoded);
   return structHash;
 };
 
@@ -297,7 +150,7 @@ const hashEIP712 = (typedData) => {
     typedData.types[typedData.primaryType],
     typedData.message,
   );
-  const hash = ethers.utils.solidityKeccak256(
+  const hash = solidityKeccak256(
     ['bytes', 'bytes32', 'bytes32'],
     ['0x1901', domainSeparatorHash, messageHash],
   );
