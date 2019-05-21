b89e87222da577bca1d631ba58bcc3cdc48377e4
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-05-21 11:11:32 +0200

    add decrypt test

--- test/inputs/opensslDecryptDataset/decryptDataset.sh
+++ test/inputs/opensslDecryptDataset/decryptDataset.sh
@@ -9,10 +9,10 @@ function decryptFile()
 		echo "Cannot find '$1.enc' or '$1.secret'."
     exit 1
 	fi
-
+	
 	openssl base64 -d -in /secrets/$1.secret -out $1.keybin
 	openssl enc -aes-256-cbc -pbkdf2 -d -in /encrypted/$1.enc -out $1.recovered -kfile $1.keybin
-  cat $1.recovered
+  exit $?
 }
 
 decryptFile $1
