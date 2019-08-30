8b5dd8f9a9fbd4a9261113ed5be0a2b474427703
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-08-30 18:27:30 +0200

    update poco-chaintest cript

--- test/poco-chaintest/createChain.sh
+++ test/poco-chaintest/createChain.sh
@@ -5,10 +5,10 @@ npm i ganache-cli
 
 mkdir mainchain
 (cd mainchain && git clone git@github.com:iExecBlockchainComputing/PoCo-dev.git)
-(cd mainchain/PoCo-dev/ && git reset --hard && git checkout v3.mainnet && git pull && npm i && ./truffle deploy)
+(cd mainchain/PoCo-dev/ && git checkout v3.mainnet && npm i && ./truffle deploy)
 
 mkdir sidechain
 (cd sidechain && git clone git@github.com:iExecBlockchainComputing/PoCo-dev.git)
-(cd sidechain/PoCo-dev/ && git reset --hard && git checkout v3.sidechain && git pull && npm i && ./truffle deploy)
+(cd sidechain/PoCo-dev/ && git checkout v3.sidechain && npm i && ./truffle deploy)
 
 docker build -t iexechub/poco-chaintest:latest .
