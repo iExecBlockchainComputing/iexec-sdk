266d065719b354eab943729e68a398ec23cde2bc
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2019-12-13 14:50:40 +0100

    add tests

---
+++ test/validators.test.js
@@ -0,0 +1,175 @@
+const BN = require('bn.js');
+const {
+  // throwIfMissing,
+  // stringSchema,
+  // uint256Schema,
+  // addressSchema,
+  // bytes32Schema,
+  // apporderSchema,
+  // signedApporderSchema,
+  // datasetorderSchema,
+  // signedDatasetorderSchema,
+  // workerpoolorderSchema,
+  // signedWorkerpoolorderSchema,
+  // requestorderSchema,
+  // signedRequestorderSchema,
+  // paramsSchema,
+  // tagSchema,
+  // chainIdSchema,
+  // hexnumberSchema,
+  positiveIntSchema,
+  positiveStrictIntSchema,
+  // appSchema,
+  // datasetSchema,
+  // categorySchema,
+  // workerpoolSchema,
+  ValidationError,
+} = require('../src/validator');
+
+// TESTS
+describe('[positiveIntSchema]', () => {
+  test('int', async () => {
+    await expect(positiveIntSchema().validate(48)).resolves.toBe(48);
+  });
+  test('string int', async () => {
+    await expect(positiveIntSchema().validate('48')).resolves.toBe(48);
+  });
+  test('hex string', async () => {
+    await expect(positiveIntSchema().validate('0xff')).resolves.toBe(255);
+  });
+  test('BN', async () => {
+    await expect(positiveIntSchema().validate(new BN(123456789))).resolves.toBe(
+      123456789,
+    );
+  });
+  test('int 0', async () => {
+    await expect(positiveIntSchema().validate(0)).resolves.toBe(0);
+  });
+  test('throw with negative int', async () => {
+    await expect(positiveIntSchema().validate(-1)).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 0'),
+    );
+  });
+  test('throw with negative string int', async () => {
+    await expect(positiveIntSchema().validate('-1')).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 0'),
+    );
+  });
+  test('throw with int string > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveIntSchema().validate('9007199254740992'),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with hex string > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveIntSchema().validate('0xffffffffffffffffffff'),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with BN > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveIntSchema().validate(new BN('9007199254740992')),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with invalid string', async () => {
+    await expect(positiveIntSchema().validate('0xfg')).rejects.toThrow(
+      new ValidationError(
+        'this must be a `number` type, but the final value was: `NaN` (cast from the value `"0xfg"`).',
+      ),
+    );
+  });
+});
+
+describe('[positiveStrictIntSchema]', () => {
+  test('int', async () => {
+    await expect(positiveStrictIntSchema().validate(48)).resolves.toBe(48);
+  });
+  test('string int', async () => {
+    await expect(
+      positiveStrictIntSchema().validate('9007199254740990'),
+    ).resolves.toBe(9007199254740990);
+  });
+  test('hex string', async () => {
+    await expect(positiveStrictIntSchema().validate('0xff')).resolves.toBe(255);
+  });
+  test('BN', async () => {
+    await expect(
+      positiveStrictIntSchema().validate(new BN(123456789)),
+    ).resolves.toBe(123456789);
+  });
+  test('throw with int 0', async () => {
+    await expect(positiveStrictIntSchema().validate(0)).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test("throw with string '0'", async () => {
+    await expect(positiveStrictIntSchema().validate('0')).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test('throw with hex string 0x0', async () => {
+    await expect(positiveStrictIntSchema().validate('0x0')).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test('throw with BN(0)', async () => {
+    await expect(positiveStrictIntSchema().validate(new BN(0))).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test('throw with negative int', async () => {
+    await expect(positiveStrictIntSchema().validate(-1)).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test('throw with negative string int', async () => {
+    await expect(positiveStrictIntSchema().validate('-1')).rejects.toThrow(
+      new ValidationError('this must be greater than or equal to 1'),
+    );
+  });
+  test('throw with int string > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveStrictIntSchema().validate('9007199254740992'),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with hex string > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveStrictIntSchema().validate('0xffffffffffffffffffff'),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with BN > MAX_SAFE_INTEGER', async () => {
+    await expect(
+      positiveStrictIntSchema().validate(new BN('9007199254740992')),
+    ).rejects.toThrow(
+      new ValidationError(
+        'this must be less than or equal to 9007199254740990',
+      ),
+    );
+  });
+  test('throw with invalid string', async () => {
+    await expect(positiveStrictIntSchema().validate('0xfg')).rejects.toThrow(
+      new ValidationError(
+        'this must be a `number` type, but the final value was: `NaN` (cast from the value `"0xfg"`).',
+      ),
+    );
+  });
+});
