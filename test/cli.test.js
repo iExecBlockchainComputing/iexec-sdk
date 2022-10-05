230c1c79c316a4343de299e714e6aec2ea647f65
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2022-10-05 14:21:09 +0200

    cli requester secret per TEE framework

--- test/cli.test.js
+++ test/cli.test.js
@@ -132,6 +132,19 @@ const setPoorWallet1 = () =>
     'wallet.json',
   );
 
+const setWallet = async (privateKey) => {
+  const wallet = privateKey
+    ? new ethers.Wallet(privateKey)
+    : ethers.Wallet.createRandom();
+  const jsonWallet = {
+    privateKey: wallet.privateKey,
+    publicKey: wallet.publicKey,
+    address: wallet.address,
+  };
+  await saveJSONToFile(jsonWallet, 'wallet.json');
+  return jsonWallet;
+};
+
 const setTokenChain = (options) =>
   saveJSONToFile(
     {
@@ -2349,6 +2362,98 @@ describe('[Mainchain]', () => {
     ).catch((e) => e.message);
     const resErr = JSON.parse(rawErr);
     expect(resErr.ok).toBe(false);
+  });
+
+  // REQUESTER
+  test('[common] requester secret', async () => {
+    await setTokenChainOpenethereum();
+    const { privateKey, address } = await setWallet();
+    // check own
+    const checkOwnNotPushed = JSON.parse(
+      await execAsync(`${iexecPath} requester check-secret foo --raw`),
+    );
+    expect(checkOwnNotPushed.ok).toBe(true);
+    expect(checkOwnNotPushed.name).toBe('foo');
+    expect(checkOwnNotPushed.isSet).toBe(false);
+
+    // push
+    const push = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester push-secret foo --secret-value FOO --raw`,
+      ),
+    );
+    expect(push.ok).toBe(true);
+    expect(push.name).toBe('foo');
+    expect(push.isPushed).toBe(true);
+    // cannot update requester secret
+    const pushUpdate = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester push-secret foo --secret-value FOOD --raw`,
+      ).catch((err) => err.message),
+    );
+    expect(pushUpdate.ok).toBe(false);
+
+    // check own pushed
+    const checkOwnPushed = JSON.parse(
+      await execAsync(`${iexecPath} requester check-secret foo --raw`),
+    );
+    expect(checkOwnPushed.ok).toBe(true);
+    expect(checkOwnPushed.name).toBe('foo');
+    expect(checkOwnPushed.isSet).toBe(true);
+
+    // anyone can check-secret
+    await removeWallet();
+    const checkPushed = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester check-secret foo ${address} --raw`,
+      ),
+    );
+    expect(checkPushed.ok).toBe(true);
+    expect(checkPushed.name).toBe('foo');
+    expect(checkPushed.isSet).toBe(true);
+
+    const checkNotPushed = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester check-secret FOO ${address} --raw`,
+      ),
+    );
+    expect(checkNotPushed.ok).toBe(true);
+    expect(checkNotPushed.name).toBe('FOO');
+    expect(checkNotPushed.isSet).toBe(false);
+
+    // check secret TEE framework validation
+    await expect(
+      execAsync(
+        `${iexecPath} requester check-secret foo ${address} --tee-framework tee --raw`,
+      ),
+    ).rejects.toThrow();
+
+    // check secret TEE framework override
+    const checkOtherFramework = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester check-secret foo ${address} --tee-framework ${TEE_FRAMEWORKS.GRAMINE} --raw`,
+      ),
+    );
+    expect(checkOtherFramework.ok).toBe(true);
+    expect(checkOtherFramework.name).toBe('foo');
+    expect(checkOtherFramework.isSet).toBe(false);
+
+    // push secret TEE framework override
+    await setWallet(privateKey);
+    const pushOtherFramework = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester push-secret foo --secret-value foo --tee-framework ${TEE_FRAMEWORKS.GRAMINE} --raw`,
+      ),
+    );
+    expect(pushOtherFramework.ok).toBe(true);
+    const checkOtherFrameworkPushed = JSON.parse(
+      await execAsync(
+        `${iexecPath} requester check-secret foo ${address} --tee-framework ${TEE_FRAMEWORKS.GRAMINE} --raw`,
+      ),
+    );
+    expect(checkOtherFrameworkPushed.ok).toBe(true);
+    expect(checkOtherFrameworkPushed.name).toBe('foo');
+    expect(checkOtherFrameworkPushed.isSet).toBe(true);
   });
 });
 
