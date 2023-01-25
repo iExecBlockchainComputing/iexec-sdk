561415990b275eb79417df48bd712ff0f746278c
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-01-25 17:11:29 +0100

    remove useless wrapping

--- src/cli/cmd/iexec.js
+++ src/cli/cmd/iexec.js
@@ -25,162 +25,154 @@ const { APP, DATASET, WORKERPOOL } = require('../../common/utils/constant');
 cli.description(packageJSON.description).version(packageJSON.version);
 cli.name('iexec').usage('[command] [options]');
 
-async function main() {
-  const init = cli.command('init');
-  addGlobalOptions(init);
-  addWalletCreateOptions(init);
-  init
-    .option(...option.force())
-    .option(...option.skipWallet())
-    .description(desc.initObj('project'))
-    .action(async (opts) => {
-      await checkUpdate(opts);
-      const spinner = Spinner(opts);
-      try {
-        const force = opts.force || opts.raw;
-        const { saved, fileName } = await initIExecConf({
-          force,
-          strict: false,
-        });
-        if (fileName) {
-          spinner.info(
-            `Here is your main config "${fileName}":${pretty(saved)}`,
-          );
-        }
-
-        const chainRes = await initChainConf({ force, strict: false });
-        if (chainRes.fileName) {
-          spinner.info(
-            `Here is your chain config "${chainRes.fileName}":${pretty(
-              chainRes.saved,
-            )}`,
-          );
-        }
-
-        let walletRes;
-        if (!opts.skipWallet) {
-          spinner.info('Creating your wallet file');
-          const walletOptions = await computeWalletCreateOptions(opts);
-          walletRes = await createAndSave({ force, ...walletOptions });
-          spinner.info(
-            `Your wallet address is ${
-              walletRes.address
-            } Wallet file saved in "${
-              walletRes.fileName
-            }" you must backup this file safely :\n${pretty(walletRes.wallet)}`,
-          );
-          spinner.warn('You must backup your wallet file in a safe place!');
-        }
-
-        const raw = {
-          ...(walletRes && { walletAddress: walletRes.address }),
-          ...(walletRes && { walletFile: walletRes.fileName }),
-          ...(fileName && { configFile: fileName }),
-          ...(chainRes && { chainConfigFile: chainRes.fileName }),
-        };
-        spinner.succeed('iExec project is ready\n', {
-          raw,
-        });
-      } catch (error) {
-        handleError(error, cli, opts);
+const init = cli.command('init');
+addGlobalOptions(init);
+addWalletCreateOptions(init);
+init
+  .option(...option.force())
+  .option(...option.skipWallet())
+  .description(desc.initObj('project'))
+  .action(async (opts) => {
+    await checkUpdate(opts);
+    const spinner = Spinner(opts);
+    try {
+      const force = opts.force || opts.raw;
+      const { saved, fileName } = await initIExecConf({
+        force,
+        strict: false,
+      });
+      if (fileName) {
+        spinner.info(`Here is your main config "${fileName}":${pretty(saved)}`);
       }
-    });
 
-  cli.command('wallet', 'manage local ethereum wallet');
+      const chainRes = await initChainConf({ force, strict: false });
+      if (chainRes.fileName) {
+        spinner.info(
+          `Here is your chain config "${chainRes.fileName}":${pretty(
+            chainRes.saved,
+          )}`,
+        );
+      }
 
-  cli.command('account', 'manage iExec account');
+      let walletRes;
+      if (!opts.skipWallet) {
+        spinner.info('Creating your wallet file');
+        const walletOptions = await computeWalletCreateOptions(opts);
+        walletRes = await createAndSave({ force, ...walletOptions });
+        spinner.info(
+          `Your wallet address is ${walletRes.address} Wallet file saved in "${
+            walletRes.fileName
+          }" you must backup this file safely :\n${pretty(walletRes.wallet)}`,
+        );
+        spinner.warn('You must backup your wallet file in a safe place!');
+      }
 
-  cli.command('app', 'manage iExec apps');
+      const raw = {
+        ...(walletRes && { walletAddress: walletRes.address }),
+        ...(walletRes && { walletFile: walletRes.fileName }),
+        ...(fileName && { configFile: fileName }),
+        ...(chainRes && { chainConfigFile: chainRes.fileName }),
+      };
+      spinner.succeed('iExec project is ready\n', {
+        raw,
+      });
+    } catch (error) {
+      handleError(error, cli, opts);
+    }
+  });
 
-  cli.command('dataset', 'manage iExec datasets');
+cli.command('wallet', 'manage local ethereum wallet');
 
-  cli.command('workerpool', 'manage iExec workerpools');
+cli.command('account', 'manage iExec account');
 
-  cli.command('requester', 'commands for the requester');
+cli.command('app', 'manage iExec apps');
 
-  cli.command('order', 'manage iExec marketplace orders');
+cli.command('dataset', 'manage iExec datasets');
 
-  cli.command('orderbook', 'show marketplace orderbook');
+cli.command('workerpool', 'manage iExec workerpools');
 
-  cli.command('deal', 'manage iExec deals');
+cli.command('requester', 'commands for the requester');
 
-  cli.command('task', 'manage iExec tasks');
+cli.command('order', 'manage iExec marketplace orders');
 
-  cli.command('storage', 'manage remote storage');
+cli.command('orderbook', 'show marketplace orderbook');
 
-  cli.command('result', 'manage results encryption');
+cli.command('deal', 'manage iExec deals');
 
-  cli.command('ens', 'manage ENS names');
+cli.command('task', 'manage iExec tasks');
 
-  cli.command('category', 'manage iExec categories');
+cli.command('storage', 'manage remote storage');
 
-  cli.command('registry', 'interact with iExec registry');
+cli.command('result', 'manage results encryption');
 
-  const infoCmd = cli.command('info');
-  addGlobalOptions(infoCmd);
-  infoCmd
-    .option(...option.chain())
-    .description(desc.info())
-    .action(async (opts) => {
-      await checkUpdate(opts);
-      const spinner = Spinner(opts);
-      try {
-        const chain = await loadChain(opts.chain, { spinner });
+cli.command('ens', 'manage ENS names');
 
-        const host =
-          chain.host ===
-          getChainDefaults({ id: chain.id, flavour: chain.flavour }).host
-            ? 'default'
-            : chain.host;
-        spinner.info(`Ethereum host: ${host}`);
+cli.command('category', 'manage iExec categories');
 
-        spinner.start(info.checking('iExec contracts info'));
+cli.command('registry', 'interact with iExec registry');
 
-        const useNative = !!chain.contracts.isNative;
-        const { flavour } = chain.contracts;
-        const rlcAddress = useNative
-          ? undefined
-          : await wrapCall(chain.contracts.fetchTokenAddress());
-        const [
+const infoCmd = cli.command('info');
+addGlobalOptions(infoCmd);
+infoCmd
+  .option(...option.chain())
+  .description(desc.info())
+  .action(async (opts) => {
+    await checkUpdate(opts);
+    const spinner = Spinner(opts);
+    try {
+      const chain = await loadChain(opts.chain, { spinner });
+
+      const host =
+        chain.host ===
+        getChainDefaults({ id: chain.id, flavour: chain.flavour }).host
+          ? 'default'
+          : chain.host;
+      spinner.info(`Ethereum host: ${host}`);
+
+      spinner.start(info.checking('iExec contracts info'));
+
+      const useNative = !!chain.contracts.isNative;
+      const { flavour } = chain.contracts;
+      const rlcAddress = useNative
+        ? undefined
+        : await wrapCall(chain.contracts.fetchTokenAddress());
+      const [
+        appRegistryAddress,
+        datasetRegistryAddress,
+        workerpoolRegistryAddress,
+      ] = await Promise.all([
+        wrapCall(chain.contracts.fetchRegistryAddress(APP)),
+        wrapCall(chain.contracts.fetchRegistryAddress(DATASET)),
+        wrapCall(chain.contracts.fetchRegistryAddress(WORKERPOOL)),
+      ]);
+      const { pocoVersion } = chain.contracts;
+
+      const iexecAddresses = {
+        'iExec PoCo version': `${pocoVersion} ${flavour}`,
+        ...((useNative && {
+          'native RLC': true,
+        }) || { 'RLC ERC20 address': rlcAddress }),
+        'iExec contract address': chain.contracts.hubAddress,
+        'app registry address': appRegistryAddress,
+        'dataset registry address': datasetRegistryAddress,
+        'workerpool registry address': workerpoolRegistryAddress,
+      };
+      spinner.succeed(`iExec contracts addresses:${pretty(iexecAddresses)}`, {
+        raw: {
+          pocoVersion,
+          host,
+          hubAddress: chain.contracts.hubAddress,
+          rlcAddress,
           appRegistryAddress,
           datasetRegistryAddress,
           workerpoolRegistryAddress,
-        ] = await Promise.all([
-          wrapCall(chain.contracts.fetchRegistryAddress(APP)),
-          wrapCall(chain.contracts.fetchRegistryAddress(DATASET)),
-          wrapCall(chain.contracts.fetchRegistryAddress(WORKERPOOL)),
-        ]);
-        const { pocoVersion } = chain.contracts;
+          useNative,
+          flavour,
+        },
+      });
+    } catch (error) {
+      handleError(error, cli, opts);
+    }
+  });
 
-        const iexecAddresses = {
-          'iExec PoCo version': `${pocoVersion} ${flavour}`,
-          ...((useNative && {
-            'native RLC': true,
-          }) || { 'RLC ERC20 address': rlcAddress }),
-          'iExec contract address': chain.contracts.hubAddress,
-          'app registry address': appRegistryAddress,
-          'dataset registry address': datasetRegistryAddress,
-          'workerpool registry address': workerpoolRegistryAddress,
-        };
-        spinner.succeed(`iExec contracts addresses:${pretty(iexecAddresses)}`, {
-          raw: {
-            pocoVersion,
-            host,
-            hubAddress: chain.contracts.hubAddress,
-            rlcAddress,
-            appRegistryAddress,
-            datasetRegistryAddress,
-            workerpoolRegistryAddress,
-            useNative,
-            flavour,
-          },
-        });
-      } catch (error) {
-        handleError(error, cli, opts);
-      }
-    });
-
-  finalizeCli(cli);
-}
-
-main();
+finalizeCli(cli);
