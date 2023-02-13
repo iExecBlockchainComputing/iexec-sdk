ae3edaa2de02db7672e3d1c3f39687c6770eaf14
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-02-13 15:43:18 +0100

    fix webpack 'utils'.'TypedDataEncoder' not found

--- src/common/utils/sig-utils.js
+++ src/common/utils/sig-utils.js
@@ -7,16 +7,10 @@ export const hashEIP712 = (typedData) => {
   try {
     const { domain, message } = typedData;
     const { EIP712Domain, ...types } = typedData.types;
-    // use experimental ether utils._TypedDataEncoder (to remove when TypedDataEncoder is included)
+    // use experimental ether utils._TypedDataEncoder (to change when TypedDataEncoder is included)
     // https://docs.ethers.io/v5/api/utils/hashing/#TypedDataEncoder
     /* eslint no-underscore-dangle: ["error", { "allow": ["_TypedDataEncoder"] }] */
-    const TypedDataEncoder =
-      utils._TypedDataEncoder &&
-      utils._TypedDataEncoder.from &&
-      typeof utils._TypedDataEncoder.from === 'function'
-        ? utils._TypedDataEncoder
-        : utils.TypedDataEncoder;
-    return TypedDataEncoder.hash(domain, types, message);
+    return utils._TypedDataEncoder.hash(domain, types, message);
   } catch (error) {
     debug('hashEIP712()', error);
     throw error;
