2d27d26346badefed6693638e678fd6c47ff13d4
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-07-11 14:13:16 +0200

    move decryptResult into result-utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,7 +7,7 @@ export {
   encodeTag,
   decodeTag,
   sumTags,
-  decryptResult,
 } from '../common/utils/utils.js';
+export { decryptResult } from '../common/utils/result-utils.js';
 export { NULL_ADDRESS, NULL_BYTES32 } from '../common/utils/constant.js';
 export { getSignerFromPrivateKey } from '../common/utils/signers.js';
