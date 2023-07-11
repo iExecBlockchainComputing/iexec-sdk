be1775a5a1b058d59d2dd8cca1979716c883dfc7
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-07-11 11:59:41 +0200

    update example bundlers configuration

--- test/browser/rollup/rollup.config.mjs
+++ test/browser/rollup/rollup.config.mjs
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import inject from '@rollup/plugin-inject';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
@@ -15,11 +13,6 @@ export default {
       preferBuiltins: false,
       browser: true,
     }),
-    commonjs(), // after @rollup/plugin-node-resolve
-    nodePolyfills(),
-    inject({
-      Buffer: ['buffer', 'Buffer'],
-      process: ['process', 'browser'],
-    }),
+    commonjs(),
   ],
 };
