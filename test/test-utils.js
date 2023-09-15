a8db2923b8f09939cb28237bebe203be444f9dec
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2023-09-15 12:41:01 +0200

    cli transfer tests

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,4 +1,5 @@
 import { exec } from 'child_process';
+import { Wallet } from 'ethers';
 
 export const execAsync = (cmd) =>
   new Promise((res, rej) => {
@@ -9,3 +10,66 @@ export const execAsync = (cmd) =>
       res(stdout + stderr);
     });
   });
+
+const { DRONE } = process.env;
+
+export const TEST_CHAINS = {
+  // // template
+  // 'chainName': {
+  //   rpcURL: 'http://___',
+  //   chainId: '1',
+  //   sconeSmsURL: 'http://___',
+  //   gramineSmsURL: 'http://___',
+  //   marketURL: 'http://___',
+  //   resultProxyURL: 'http://___',
+  //   hubAddress: '0x___',
+  //   enterpriseHubAddress: '0x___',
+  //   ensRegistryAddress: '0x___',
+  //   ensPublicResolverAddress: '0x___',
+  //   richWallet: '0x___',
+  // },
+
+  // autoseal chain with iExec token
+  token: {
+    rpcURL: DRONE ? 'http://token-chain:8545' : 'http://localhost:8545',
+    chainId: '65535',
+    sconeSmsURL: DRONE
+      ? 'http://token-sms-scone:13300'
+      : 'http://localhost:13301',
+    gramineSmsURL: DRONE
+      ? 'http://token-sms-gramine:13300'
+      : 'http://localhost:13302',
+    marketURL: undefined, // no market connected
+    resultProxyURL: DRONE
+      ? 'http://token-result-proxy:13200'
+      : 'http://localhost:13200',
+    hubAddress: '0xC129e7917b7c7DeDfAa5Fff1FB18d5D7050fE8ca',
+    enterpriseHubAddress: '0xb80C02d24791fA92fA8983f15390274698A75D23',
+    ensRegistryAddress: '0xaf87b82B01E484f8859c980dE69eC8d09D30F22a',
+    ensPublicResolverAddress: '0x464E9FC01C2970173B183D24B43A0FA07e6A072E',
+    richWallet:
+      '0x564a9db84969c8159f7aa3d5393c5ecd014fce6a375842a45b12af6677b12407',
+  },
+};
+
+Object.keys(TEST_CHAINS).forEach((chain) => {
+  if (!TEST_CHAINS[chain].smsMap) {
+    TEST_CHAINS[chain].smsMap = {
+      gramine: TEST_CHAINS[chain].gramineSmsURL,
+      scone: TEST_CHAINS[chain].sconeSmsURL,
+    };
+  }
+});
+
+let sequenceId = Date.now();
+export const getId = () => {
+  sequenceId += 1;
+  return sequenceId;
+};
+
+export const getRandomWallet = () => {
+  const { privateKey, publicKey, address } = Wallet.createRandom();
+  return { privateKey, publicKey, address };
+};
+
+export const getRandomAddress = () => getRandomWallet().address;
