2a7455370aa71dcbea48ca1581c4c8a936a1c1fb
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-01-24 17:25:45 +0100

    bump 8.5.2

--- src/common/generated/sdk/package.js
+++ src/common/generated/sdk/package.js
@@ -1,6 +1,6 @@
 // this file is auto generated do not edit it
 /* eslint-disable */
 export const name = "iexec";
-export const version = "8.5.1";
+export const version = "8.5.2";
 export const description = "iExec SDK";
 export default { name, version, description };
