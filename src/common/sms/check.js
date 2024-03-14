ff3b4e5b74df06624475aa697fa625ebd01d7d80
luisccastillo <luiscarloscastillo@protonmail.com>
2024-03-14 19:22:30 +0100

    cache secret checks in memory

--- src/common/sms/check.js
+++ src/common/sms/check.js
@@ -8,6 +8,20 @@ import {
 
 const debug = Debug('iexec:sms:check');
 
+let secretOkCache;
+const CACHE_TTL_MILLISECONDS = 60000;
+
+const initializeTTLCache = () => {
+  secretOkCache = [
+    [], // web3 secrets
+    {}, // requester secrets
+    [], // app secrets
+  ];
+  setTimeout(() => {
+    secretOkCache = undefined;
+  }, CACHE_TTL_MILLISECONDS);
+};
+
 // used for dataset key
 export const checkWeb3SecretExists = async (
   contracts = throwIfMissing(),
@@ -18,6 +32,12 @@ export const checkWeb3SecretExists = async (
     const vResourceAddress = await addressSchema({
       ethProvider: contracts.provider,
     }).validate(resourceAddress);
+    if (!secretOkCache) {
+      initializeTTLCache();
+    }
+    if (secretOkCache[0].includes(resourceAddress)) {
+      return true;
+    }
     const res = await httpRequest('HEAD')({
       api: smsURL,
       endpoint: '/secrets/web3',
@@ -29,6 +49,7 @@ export const checkWeb3SecretExists = async (
       throw Error(`SMS at ${smsURL} didn't answered`);
     });
     if (res.ok) {
+      secretOkCache[0].push(resourceAddress);
       return true;
     }
     if (res.status === 404) {
@@ -90,6 +111,12 @@ export const checkRequesterSecretExists = async (
     const vRequesterAddress = await addressSchema({
       ethProvider: contracts.provider,
     }).validate(requesterAddress);
+    if (!secretOkCache) {
+      initializeTTLCache();
+    }
+    if (secretOkCache[1][requesterAddress]?.includes(secretName)) {
+      return true;
+    }
     const res = await httpRequest('HEAD')({
       api: smsURL,
       endpoint: `/requesters/${vRequesterAddress}/secrets/${secretName}`,
@@ -98,6 +125,10 @@ export const checkRequesterSecretExists = async (
       throw Error(`SMS at ${smsURL} didn't answered`);
     });
     if (res.ok) {
+      // eslint-disable-next-line no-unused-expressions
+      secretOkCache[1][requesterAddress]
+        ? secretOkCache[1][requesterAddress].push(secretName)
+        : (secretOkCache[1][requesterAddress] = [secretName]);
       return true;
     }
     if (res.status === 404) {
@@ -123,6 +154,12 @@ export const checkAppSecretExists = async (
       ethProvider: contracts.provider,
     }).validate(appAddress);
     const vSecretIndex = await positiveIntSchema().validate(secretIndex);
+    if (!secretOkCache) {
+      initializeTTLCache();
+    }
+    if (secretOkCache[2].includes(appAddress)) {
+      return true;
+    }
     const res = await httpRequest('HEAD')({
       api: smsURL,
       endpoint: `/apps/${vAppAddress}/secrets/${vSecretIndex}`,
@@ -131,6 +168,7 @@ export const checkAppSecretExists = async (
       throw Error(`SMS at ${smsURL} didn't answered`);
     });
     if (res.ok) {
+      secretOkCache[2].push(appAddress);
       return true;
     }
     if (res.status === 404) {
