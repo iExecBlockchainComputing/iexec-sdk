96d018955438892b0d3226cd6a2b2b096a715ca6
Abbes Benayache <benayache.abbes@gmail.com>
2024-05-27 18:54:01 +0200

    impliment showUserVoucher func

--- src/common/voucher/voucher.js
+++ src/common/voucher/voucher.js
@@ -1,8 +1,12 @@
 import Debug from 'debug';
 import { Contract } from 'ethers';
 import { abi } from './abi/Voucher.js';
-import { throwIfMissing } from '../utils/validator.js';
+import { addressSchema, throwIfMissing } from '../utils/validator.js';
 import { fetchVoucherAddress } from './voucherHub.js';
+import { getGraphQLClient } from '../utils/graphql-utils.js';
+import { checkAllowance } from '../account/allowance.js';
+import { getVoucherAuthorizedAccounts } from './subgraph/authorizedAccounts.js';
+import { getVoucherSponsoredAssets } from './subgraph/sponsoredAssets.js';
 
 const debug = Debug('iexec:voucher:voucher');
 
@@ -30,4 +34,64 @@ export const fetchVoucherContract = async (
     debug('fetchVoucherContract()', error);
     throw error;
   }
+};
+
+export const showUserVoucher = async (
+  contracts = throwIfMissing(),
+  voucherSubgraphURL = throwIfMissing(),
+  voucherHubAddress = throwIfMissing(),
+  owner = throwIfMissing(),
+) => {
+  const vOwner = await addressSchema({
+    ethProvider: contracts.provider,
+  }).validate(owner);
+  const voucherAddress = await fetchVoucherAddress(
+    contracts,
+    voucherHubAddress,
+    vOwner,
+  );
+
+  if (!voucherAddress) {
+    throw new Error('No voucher found for this user');
+  }
+  const voucherContract = getVoucherContract(contracts, voucherAddress);
+
+  const type = await voucherContract.getType();
+  const balance = await voucherContract.getBalance();
+  const expirationTimestamp = await voucherContract.getExpiration();
+  const allowanceAmount = await checkAllowance(
+    contracts,
+    vOwner,
+    voucherAddress,
+  );
+  const graphQLClient = getGraphQLClient(voucherSubgraphURL);
+
+  const authorizedAccounts = await getVoucherAuthorizedAccounts(
+    graphQLClient,
+    voucherAddress,
+  );
+  const sponsoredAssets = await getVoucherSponsoredAssets(
+    graphQLClient,
+    voucherAddress,
+  );
+  const sponsoredApps = sponsoredAssets.filter((asset) => asset.type === 'app');
+  const sponsoredDatasets = sponsoredAssets.filter(
+    (asset) => asset.type === 'dataset',
+  );
+  const sponsoredWorkerpools = sponsoredAssets.filter(
+    (asset) => asset.type === 'workerpool',
+  );
+
+  return {
+    owner,
+    address: voucherAddress,
+    type,
+    balance,
+    expirationTimestamp,
+    sponsoredApps,
+    sponsoredDatasets,
+    sponsoredWorkerpools,
+    allowanceAmount,
+    authorizedAccounts,
+  };
 };
--- src/lib/IExecVoucherModule.js
+++ src/lib/IExecVoucherModule.js
@@ -1,5 +1,6 @@
 import IExecModule from './IExecModule.js';
 import { fetchVoucherAddress } from '../common/voucher/voucherHub.js';
+import { showUserVoucher } from '../common/voucher/voucher.js';
 
 export default class IExecVoucherModule extends IExecModule {
   constructor(...args) {
@@ -10,6 +11,19 @@ export default class IExecVoucherModule extends IExecModule {
       const voucherHubAddress = await this.config.resolveVoucherHubAddress();
 
       return fetchVoucherAddress(contracts, voucherHubAddress, owner);
+    };
+
+    this.showUserVoucher = async (owner) => {
+      const contracts = await this.config.resolveContractsClient();
+      const voucherHubAddress = await this.config.resolveVoucherHubAddress();
+      const voucherSubgraphURL = await this.config.resolveVoucherSubgraphURL();
+
+      return showUserVoucher(
+        contracts,
+        voucherSubgraphURL,
+        voucherHubAddress,
+        owner,
+      );
     };
   }
 }
