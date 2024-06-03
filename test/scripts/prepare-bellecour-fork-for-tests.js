2d3a0c597afb251b68481d1f27a9c951f0749781
Abbes Benayache <benayache.abbes@gmail.com>
2024-06-03 19:29:27 +0200

    remove export of impersonate and stopImpersonate functions

--- test/scripts/prepare-bellecour-fork-for-tests.js
+++ test/scripts/prepare-bellecour-fork-for-tests.js
@@ -43,7 +43,7 @@ const setBalance = async (address, weiAmount) => {
   console.log(`${address} wallet balance is now ${formatEther(balance)} RLC`);
 };
 
-export const impersonate = async (address) => {
+const impersonate = async (address) => {
   await fetch(rpcURL, {
     method: 'POST',
     body: JSON.stringify({
@@ -59,7 +59,7 @@ export const impersonate = async (address) => {
   console.log(`impersonating ${address}`);
 };
 
-export const stopImpersonate = async (address) => {
+const stopImpersonate = async (address) => {
   await fetch(rpcURL, {
     method: 'POST',
     body: JSON.stringify({
