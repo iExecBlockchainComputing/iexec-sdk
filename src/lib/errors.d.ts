75d1d27bfd086c54e62e640e922aced3910fb6d6
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-06-18 17:33:05 +0200

    typo

--- src/lib/errors.d.ts
+++ src/lib/errors.d.ts
@@ -8,7 +8,7 @@ export class ValidationError extends YupValidationError {}
  */
 export class ConfigurationError extends Error {}
 /**
- * Web3ProviderError encapsulate an error thrown by the web3 provider.
+ * Web3ProviderError encapsulates an error thrown by the web3 provider.
  */
 export class Web3ProviderError extends Error {
   constructor(message: string, originalError: Error);
@@ -19,15 +19,15 @@ export class Web3ProviderError extends Error {
   cause?: Error;
 }
 /**
- * Web3ProviderCallError encapsulate an error thrown by the web3 provider during a web3 call.
+ * Web3ProviderCallError encapsulates an error thrown by the web3 provider during a web3 call.
  */
 export class Web3ProviderCallError extends Web3ProviderError {}
 /**
- * Web3ProviderSendError encapsulate an error thrown by the web3 provider during a transaction.
+ * Web3ProviderSendError encapsulates an error thrown by the web3 provider during a transaction.
  */
 export class Web3ProviderSendError extends Web3ProviderError {}
 /**
- * Web3ProviderSignMessageError encapsulate an error thrown by the web3 provider during a message signature.
+ * Web3ProviderSignMessageError encapsulates an error thrown by the web3 provider during a message signature.
  */
 export class Web3ProviderSignMessageError extends Web3ProviderError {}
 /**
@@ -52,7 +52,7 @@ export class BridgeError extends Error {
 }
 
 /**
- * ApiCallError encapsulate an error occurring during a call to an API such as a network error or a server internal error.
+ * ApiCallError encapsulates an error occurring during a call to an API such as a network error or a server internal error.
  */
 export class ApiCallError extends Error {
   constructor(message: string, originalError: Error);
@@ -64,26 +64,26 @@ export class ApiCallError extends Error {
 }
 
 /**
- * SmsCallError encapsulate an error occurring during a call to the SMS API such as a network error or a server internal error.
+ * SmsCallError encapsulates an error occurring during a call to the SMS API such as a network error or a server internal error.
  */
 export class SmsCallError extends ApiCallError {}
 
 /**
- * ResultProxyCallError encapsulate an error occurring during a call to the Result Proxy API such as a network error or a server internal error.
+ * ResultProxyCallError encapsulates an error occurring during a call to the Result Proxy API such as a network error or a server internal error.
  */
 export class ResultProxyCallError extends ApiCallError {}
 
 /**
- * MarketCallError encapsulate an error occurring during a call to the Market API such as a network error or a server internal error.
+ * MarketCallError encapsulates an error occurring during a call to the Market API such as a network error or a server internal error.
  */
 export class MarketCallError extends ApiCallError {}
 
 /**
- * IpfsGatewayCallError encapsulate an error occurring during a call to the IPFS gateway API such as a network error or a server internal error.
+ * IpfsGatewayCallError encapsulates an error occurring during a call to the IPFS gateway API such as a network error or a server internal error.
  */
 export class IpfsGatewayCallError extends ApiCallError {}
 
 /**
- * WorkerpoolCallError encapsulate an error occurring during a call to a workerpool API such as a network error or a server internal error.
+ * WorkerpoolCallError encapsulates an error occurring during a call to a workerpool API such as a network error or a server internal error.
  */
 export class WorkerpoolCallError extends ApiCallError {}
