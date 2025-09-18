c039448c9ac1e463dd210ca739d5eb7b704ef106
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2025-09-18 17:53:42 +0200

    feat: add prepareDatasetBulk

--- src/common/utils/config.js
+++ src/common/utils/config.js
@@ -16,6 +16,7 @@ const networkConfigs = [
     },
     resultProxy: 'https://result.v8-bellecour.iex.ec',
     ipfsGateway: 'https://ipfs-gateway.v8-bellecour.iex.ec',
+    ipfsNode: 'https://ipfs-upload.v8-bellecour.iex.ec',
     iexecGateway: 'https://api.market.v8-bellecour.iex.ec',
     compass: undefined, // no compass using ENS
     pocoSubgraph: 'https://thegraph.iex.ec/subgraphs/name/bellecour/poco-v5',
@@ -39,6 +40,7 @@ const networkConfigs = [
     sms: undefined, // no protocol running
     resultProxy: undefined, // no protocol running
     ipfsGateway: undefined, // no protocol running
+    ipfsNode: undefined, // no protocol running
     iexecGateway: undefined, // no protocol running
     compass: undefined, // no protocol running
     pocoSubgraph: undefined, // no protocol running
@@ -63,6 +65,7 @@ const networkConfigs = [
     },
     resultProxy: undefined, // not exposed
     ipfsGateway: 'https://ipfs-gateway.arbitrum-sepolia-testnet.iex.ec',
+    ipfsNode: 'https://ipfs-upload.arbitrum-sepolia-testnet.iex.ec',
     iexecGateway: 'https://api-market.arbitrum-sepolia-testnet.iex.ec',
     compass: 'https://compass.arbitrum-sepolia-testnet.iex.ec',
     pocoSubgraph:
@@ -85,6 +88,7 @@ const networkConfigs = [
     },
     resultProxy: undefined, // not exposed
     ipfsGateway: 'https://ipfs-gateway.arbitrum-mainnet.iex.ec',
+    ipfsNode: 'https://ipfs-upload.arbitrum-mainnet.iex.ec',
     iexecGateway: 'https://api.market.arbitrum-mainnet.iex.ec',
     compass: 'https://compass.arbitrum-mainnet.iex.ec',
     pocoSubgraph:
@@ -117,6 +121,7 @@ export const getChainDefaults = (
     resultProxy,
     iexecGateway,
     ipfsGateway,
+    ipfsNode,
     compass,
     pocoSubgraph,
     voucherHub,
@@ -139,6 +144,7 @@ export const getChainDefaults = (
     resultProxy,
     iexecGateway,
     ipfsGateway,
+    ipfsNode,
     compass,
     pocoSubgraph,
     voucherHub,
